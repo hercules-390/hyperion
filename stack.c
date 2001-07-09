ff306a31d92deba38eb0d4d379514bd11c86b717
Paul Leisy <ptl00>
2001-07-09 23:47:04 +0000

    Fix TRAP so it receives PIC13 in REAL mode
    git-svn-id: file:///home/jj/hercules.svn/trunk@176 956126f8-22a0-4046-8f4a-272fa8102e63

--- stack.c
+++ stack.c
@@ -126,8 +126,9 @@ U32  trap_flags;
 QWORD trap_psw;
 int  i;
 
-    if(!PRIMARY_SPACE_MODE(&(regs->psw)) 
-      && !ACCESS_REGISTER_MODE(&(regs->psw)))
+    if (   REAL_MODE(&regs->psw)
+      || !(PRIMARY_SPACE_MODE(&regs->psw) 
+      ||   ACCESS_REGISTER_MODE(&regs->psw)) )
         ARCH_DEP(program_interrupt) (regs, PGM_SPECIAL_OPERATION_EXCEPTION);
 
     /* Obtain the DUCT origin from control register 2 */
