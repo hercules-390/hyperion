cd61be44057371dc862a20a6407cd03e9e5408e0
Paul Leisy <ptl00>
2001-07-27 16:58:09 +0000

    TRAP missing key-controlled protection. Added new routine called abs_trap_addr() which is similiar to abs_stack_addr() except it also checks keys for protection. - Paul Leisy
    git-svn-id: file:///home/jj/hercules.svn/trunk@224 956126f8-22a0-4046-8f4a-272fa8102e63

--- opcode.h
+++ opcode.h
@@ -1328,6 +1328,7 @@ void ARCH_DEP(sie_exit) (REGS *regs, int code);
 
 /* Functions in module stack.c */
 void ARCH_DEP(trap_x) (int trap_is_trap4, int execflag, REGS *regs, U32 trap_operand);
+RADR ARCH_DEP(abs_trap_addr) (VADR vaddr, REGS *regs, int acctype);
 RADR ARCH_DEP(abs_stack_addr) (VADR vaddr, REGS *regs, int acctype);
 void ARCH_DEP(form_stack_entry) (BYTE etype, VADR retna, VADR calla,
 	U32 csi, U32 pcnum, REGS *regs);
