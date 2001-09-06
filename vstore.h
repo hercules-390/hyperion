b39a97390cb4e33843e3c02206822f9dcc95ac12
Paul Leisy <ptl00>
2001-09-06 20:29:05 +0000

    Fetches to storage did not have FPO crosser check. If an operand began in FPO area (0 to 7FF) and crossed over into non-FPO area (800 or above), it could miss getting a protection check (PIC 04).
    Added FPO crosser checks to vfetch2(), vfetch4(), vfetch8(), vfetchc()
    and move_chars(). - Paul Leisy
    
    
    git-svn-id: file:///home/jj/hercules.svn/trunk@275 956126f8-22a0-4046-8f4a-272fa8102e63

--- vstore.h
+++ vstore.h
@@ -266,6 +266,7 @@ _VSTORE_C_STATIC void ARCH_DEP(vfetchc) (void *dest, BYTE len,
                                         VADR addr, int arn, REGS *regs)
 {
 VADR    addr2;                          /* Page address of last byte */
+RADR    abs, abs2;                      /* Absolute addresses        */
 BYTE    len1;                           /* Length to end of page     */
 BYTE    len2;                           /* Length after next page    */
 BYTE    akey;                           /* Bits 0-3=key, 4-7=zeroes  */
@@ -277,18 +278,24 @@ BYTE    akey;                           /* Bits 0-3=key, 4-7=zeroes  */
     addr2 = (addr + len) & ADDRESS_MAXWRAP(regs);
     addr2 &= PAGEFRAME_PAGEMASK;
 
-    /* Copy data from real storage in either one or two parts
-       depending on whether operand crosses a page boundary */
-    if (addr2 == (addr & PAGEFRAME_PAGEMASK)) {
-        addr = LOGICAL_TO_ABS (addr, arn, regs, ACCTYPE_READ, akey);
-        memcpy (dest, sysblk.mainstor+addr, len+1);
-    } else {
+    /* If no page crosser, fetch once from same absolute page */
+    if (addr2 == (addr & PAGEFRAME_PAGEMASK))
+    {
+        abs = LOGICAL_TO_ABS (addr, arn, regs, ACCTYPE_READ, akey);
+        memcpy (dest, sysblk.mainstor+abs, len+1);
+
+        /* Check protection on last byte if FPO crosser */
+        if (addr > 0x7FF || addr + len < 0x800) return;
+        abs = LOGICAL_TO_ABS (addr + len, arn, regs, ACCTYPE_READ, akey);
+    }
+    else
+    {
         len1 = addr2 - addr;
         len2 = len - len1 + 1;
-        addr = LOGICAL_TO_ABS (addr, arn, regs, ACCTYPE_READ, akey);
-        addr2 = LOGICAL_TO_ABS (addr2, arn, regs, ACCTYPE_READ, akey);
-        memcpy (dest, sysblk.mainstor+addr, len1);
-        memcpy (dest+len1, sysblk.mainstor+addr2, len2);
+        abs = LOGICAL_TO_ABS (addr, arn, regs, ACCTYPE_READ, akey);
+        abs2 = LOGICAL_TO_ABS (addr2, arn, regs, ACCTYPE_READ, akey);
+        memcpy (dest, sysblk.mainstor+abs, len1);
+        memcpy (dest+len1, sysblk.mainstor+abs2, len2);
     }
 } /* end function ARCH_DEP(vfetchc) */
 
@@ -331,26 +338,23 @@ _VSTORE_C_STATIC BYTE ARCH_DEP(vfetchb) (VADR addr, int arn,
 _VSTORE_C_STATIC U16 ARCH_DEP(vfetch2) (VADR addr, int arn, REGS *regs)
 {
 VADR    addr2;                          /* Address of second byte    */
-RADR    abs1, abs2;                     /* Absolute addresses        */
-BYTE    akey;                           /* Bits 0-3=key, 4-7=zeroes  */
+RADR    abs, abs2;                      /* Absolute addresses        */
 
-    /* Obtain current access key from PSW */
-    akey = regs->psw.pkey;
+    /* Check to see if operand crosses page or FPO boundary */
+    if (!(addr & 1) || ((addr & PAGEFRAME_BYTEMASK) <= (PAGEFRAME_PAGESIZE - 2) &&
+                                                       (addr != 0x7FF)))
+    {
+        abs = LOGICAL_TO_ABS (addr, arn, regs, ACCTYPE_READ, regs->psw.pkey);
+        return CSWAP16(*((U16*)(sysblk.mainstor + abs)));
+    }
 
-    /* Get absolute address of first byte of operand */
-    abs1 = LOGICAL_TO_ABS (addr, arn, regs, ACCTYPE_READ, akey);
-
-    if(!(addr & 1) || (abs1 & PAGEFRAME_BYTEMASK) <= (PAGEFRAME_PAGESIZE - 2))
-        return CSWAP16(*((U16*)(sysblk.mainstor + abs1)));
-
-    /* Calculate address of second byte of operand */
+    /* Page or FPO crosser, get absolute address of first and last byte */
     addr2 = (addr + 1) & ADDRESS_MAXWRAP(regs);
-
-    /* Repeat address translation for 2nd page */
-    abs2 = LOGICAL_TO_ABS (addr2, arn, regs, ACCTYPE_READ, akey);
+    abs = LOGICAL_TO_ABS (addr, arn, regs, ACCTYPE_READ, regs->psw.pkey);
+    abs2 = LOGICAL_TO_ABS (addr2, arn, regs, ACCTYPE_READ, regs->psw.pkey);
 
     /* Return integer value of operand */
-    return (sysblk.mainstor[abs1] << 8) | sysblk.mainstor[abs2];
+    return (sysblk.mainstor[abs] << 8) | sysblk.mainstor[abs2];
 } /* end function ARCH_DEP(vfetch2) */
 
 /*-------------------------------------------------------------------*/
@@ -373,24 +377,20 @@ int     i;                              /* Loop counter              */
 U32     value;                          /* Accumulated value         */
 VADR    addr2;                          /* Page address of last byte */
 RADR    abs, abs2;                      /* Absolute addresses        */
-BYTE    akey;                           /* Bits 0-3=key, 4-7=zeroes  */
 
-    /* Obtain current access key from PSW */
-    akey = regs->psw.pkey;
-
-    /* Get absolute address of first byte of operand */
-    abs = LOGICAL_TO_ABS (addr, arn, regs, ACCTYPE_READ, akey);
-
-    /* Fetch 4 bytes when operand does not cross page boundary */
-    if(!(addr & 3) || (abs & PAGEFRAME_BYTEMASK) <= (PAGEFRAME_PAGESIZE - 4))
+    /* Check to see if operand crosses page or FPO boundary */
+    if (!(addr & 3) || ((addr & PAGEFRAME_BYTEMASK) <= (PAGEFRAME_PAGESIZE - 4) &&
+                                     (addr > 0x7FF || addr < 0x7FD)))
+    {
+        abs = LOGICAL_TO_ABS (addr, arn, regs, ACCTYPE_READ, regs->psw.pkey);
         return CSWAP32(*((U32*)(sysblk.mainstor + abs)));
+    }
 
-    /* Operand is not fullword aligned and may cross a page boundary */
-
-    /* Calculate page address of last byte of operand */
+    /* Page or FPO crosser, get absolute address of first and last byte */
     addr2 = (addr + 3) & ADDRESS_MAXWRAP(regs);
-    addr2 &= PAGEFRAME_PAGEMASK;
-    abs2 = LOGICAL_TO_ABS (addr2, arn, regs, ACCTYPE_READ, akey);
+    addr2 &= 0x7FFFF800;  /* round to nearest page or FPO boundary */
+    abs = LOGICAL_TO_ABS (addr, arn, regs, ACCTYPE_READ, regs->psw.pkey);
+    abs2 = LOGICAL_TO_ABS (addr2, arn, regs, ACCTYPE_READ, regs->psw.pkey);
 
     /* Fetch integer value byte by byte from operand location */
     for (i=0, value=0; i < 4; i++) {
@@ -433,22 +433,20 @@ int     i;                              /* Loop counter              */
 U64     value;                          /* Accumulated value         */
 VADR    addr2;                          /* Page address of last byte */
 RADR    abs, abs2;                      /* Absolute addresses        */
-BYTE    akey;                           /* Bits 0-3=key, 4-7=zeroes  */
 
-    /* Obtain current access key from PSW */
-    akey = regs->psw.pkey;
-
-    /* Get absolute address of first byte of operand */
-    abs = LOGICAL_TO_ABS (addr, arn, regs, ACCTYPE_READ, akey);
-
-    /* Fetch 8 bytes when operand does not cross page boundary */
-    if(!(addr & 7) || (abs & PAGEFRAME_BYTEMASK) <= (PAGEFRAME_PAGESIZE - 8))
+    /* Check to see if operand crosses page or FPO boundary */
+    if (!(addr & 7) || ((addr & PAGEFRAME_BYTEMASK) <= (PAGEFRAME_PAGESIZE - 8) &&
+                                                 (addr > 0x7FF || addr < 0x7F9)))
+    {
+        abs = LOGICAL_TO_ABS (addr, arn, regs, ACCTYPE_READ, regs->psw.pkey);
         return CSWAP64(*((U64*)(sysblk.mainstor + abs)));
+    }
 
-    /* Calculate page address of last byte of operand */
+    /* Page or FPO crosser, get absolute address of first and last byte */
     addr2 = (addr + 7) & ADDRESS_MAXWRAP(regs);
-    addr2 &= PAGEFRAME_PAGEMASK;
-    abs2 = LOGICAL_TO_ABS (addr2, arn, regs, ACCTYPE_READ, akey);
+    addr2 &= 0x7FFFF800;  /* round to nearest page or FPO boundary */
+    abs = LOGICAL_TO_ABS (addr, arn, regs, ACCTYPE_READ, regs->psw.pkey);
+    abs2 = LOGICAL_TO_ABS (addr2, arn, regs, ACCTYPE_READ, regs->psw.pkey);
 
     /* Fetch integer value byte by byte from operand location */
     for (i=0, value=0; i < 8; i++) {
@@ -609,6 +607,10 @@ BYTE    slow = 0;
     npv1 &= PAGEFRAME_PAGEMASK;
     npv2 = (addr2 + len) & ADDRESS_MAXWRAP(regs);
     npv2 &= PAGEFRAME_PAGEMASK;
+
+    /* If FPO crosser, check last byte for protection */
+    if (addr2 < 0x800 && addr2 + len >= 0x800)
+        npa2 = LOGICAL_TO_ABS (addr2 + len, arn2, regs, ACCTYPE_READ, key2);
 
     /* Translate next page addresses if page boundary crossed */
     if (npv1 != (addr1 & PAGEFRAME_PAGEMASK))
