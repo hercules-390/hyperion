13d7755e1c9e4df5cb9a100d07c39c81540e68a6
Matt Zimmerman <mdz>
2001-10-07 22:32:21 +0000

    Header to get us the int*_t and uint*_t types, regardless of platform
    git-svn-id: file:///home/jj/hercules.svn/trunk@329 956126f8-22a0-4046-8f4a-272fa8102e63

---
+++ htypes.h
@@ -0,0 +1,17 @@
+#ifdef HAVE_CONFIG_H
+#include <config.h>
+#endif
+
+#ifdef HAVE_INTTYPES_H
+#include <inttypes.h>
+#elif defined(HAVE_U_INT)
+#ifdef HAVE_SYS_TYPES_H
+#include <sys/types.h>
+#endif
+#define uint8_t u_int8_t
+#define uint16_t u_int6_t
+#define uint32_t u_int32_t
+#define uint64_t u_int64_t
+#else
+#error Unable to find fixed-size data types
+#endif
