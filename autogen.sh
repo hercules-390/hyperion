a94f059fc2b29fb76d66fccb70a012aeba57ba0c
Jan Jaeger <jj>
2002-06-02 11:28:40 +0000

    A few mods to fix autoconf problems with gettext
    git-svn-id: file:///home/jj/hercules.svn/trunk@906 956126f8-22a0-4046-8f4a-272fa8102e63

--- autogen.sh
+++ autogen.sh
@@ -1,6 +1,5 @@
 #!/bin/sh
 if test ! -e ./ABOUT-NLS; then
   gettextize --force --no-changelog > /dev/null
-  mv configure.ac\~ configure.ac
 fi
-aclocal -I m4 && autoconf && autoheader && automake --add-missing
+aclocal && autoconf && autoheader && automake --add-missing
