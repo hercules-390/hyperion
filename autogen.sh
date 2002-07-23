5d943b9721c97218af86fd32658f080076e0f3d9
Matt Zimmerman <mdz>
2002-07-23 13:55:47 +0000

    Be smarter about potential failures
    git-svn-id: file:///home/jj/hercules.svn/trunk@986 956126f8-22a0-4046-8f4a-272fa8102e63

--- autogen.sh
+++ autogen.sh
@@ -1,5 +1,12 @@
 #!/bin/sh
+
+# Fail if there are any errors -mdz
+set -e
+
 if test ! -e ./ABOUT-NLS; then
   gettextize --force --no-changelog > /dev/null
 fi
+
 aclocal && autoconf && autoheader && automake --add-missing
+
+echo "Completed successfully"
