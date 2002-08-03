4af1c05f71b08dcc257acc21b528bbb03627d1b6
Volker Bandke <vbandke>
2002-08-03 08:25:00 +0000

    make NLS support work for Cygwin
    git-svn-id: file:///home/jj/hercules.svn/trunk@994 956126f8-22a0-4046-8f4a-272fa8102e63

--- autogen.sh
+++ autogen.sh
@@ -2,14 +2,16 @@
 
 # Fail if there are any errors -mdz
 set -e
-
+rm -f ./autogen.log
 if test ! -e ./ABOUT-NLS; then
-  gettextize --force --no-changelog > /dev/null
+  gettextize --force --no-changelog --intl >>./autogen.log 2>&1
+  aclocal -I m4 >>./autogen.log 2>&1
+  automake m4/Makefile >>./autogen.log 2>&1
 fi
 
-aclocal
-autoconf
-autoheader
-automake --add-missing
+aclocal >>./autogen.log 2>&1
+autoconf >>./autogen.log 2>&1
+autoheader >>./autogen.log 2>&1
+automake --add-missing >>./autogen.log 2>&1
 
 echo "Completed successfully"
