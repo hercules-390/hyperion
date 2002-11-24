936d9e7ced5779a67f4d247b0fcdadd3918c0169
Matt Zimmerman <mdz>
2002-11-24 15:08:40 +0000

    Add message pointing to autogen.log
    git-svn-id: file:///home/jj/hercules.svn/trunk@1050 956126f8-22a0-4046-8f4a-272fa8102e63

--- autogen.sh
+++ autogen.sh
@@ -2,6 +2,9 @@
 
 # Fail if there are any errors -mdz
 set -e
+
+echo "Output goes to autogen.log"
+
 rm -f ./autogen.log
 if test ! -e ./ABOUT-NLS; then
   gettextize --force --no-changelog --intl >>./autogen.log 2>&1
