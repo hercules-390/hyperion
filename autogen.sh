699db968fd7d5de76faddfa6e7d7ada54ed83e28
Volker Bandke <vbandke>
2002-12-16 13:11:44 +0000

    Fixed a typo
    git-svn-id: file:///home/jj/hercules.svn/trunk@1071 956126f8-22a0-4046-8f4a-272fa8102e63

--- autogen.sh
+++ autogen.sh
@@ -8,8 +8,8 @@ echo "Output goes to autogen.log"
 rm -f ./autogen.log
 if test ! -e ./ABOUT-NLS; then
   gettextize --force --no-changelog --intl | tee ./autogen.log 2>&1
-  echo -e "\n\aIn spite of the confirmation required from you you do NOT have"
-  echo -e "to run the indicated programs, as we will automagically run them"
+  echo -e "\nIn spite of the confirmation required from you you do NOT have"
+  echo -e "to run the indicated program(s), as we will automagically run them"
   echo -e "for you now"
   echo -e "     aclocal -I m4"
   aclocal -I m4        | tee -a ./autogen.log 2>&1
