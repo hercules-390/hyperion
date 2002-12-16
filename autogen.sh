3220b4942308864829b97f1172a3e1a97114f323
Volker Bandke <vbandke>
2002-12-16 12:44:25 +0000

    Make the gettextize confirmation prompt visible
    git-svn-id: file:///home/jj/hercules.svn/trunk@1070 956126f8-22a0-4046-8f4a-272fa8102e63

--- autogen.sh
+++ autogen.sh
@@ -7,14 +7,19 @@ echo "Output goes to autogen.log"
 
 rm -f ./autogen.log
 if test ! -e ./ABOUT-NLS; then
-  gettextize --force --no-changelog --intl >>./autogen.log 2>&1
-  aclocal -I m4 >>./autogen.log 2>&1
-  automake m4/Makefile >>./autogen.log 2>&1
+  gettextize --force --no-changelog --intl | tee ./autogen.log 2>&1
+  echo -e "\n\aIn spite of the confirmation required from you you do NOT have"
+  echo -e "to run the indicated programs, as we will automagically run them"
+  echo -e "for you now"
+  echo -e "     aclocal -I m4"
+  aclocal -I m4        | tee -a ./autogen.log 2>&1
+  echo -e "     automake m4/Makefile"
+  automake m4/Makefile | tee -a ./autogen.log 2>&1
 fi
 
-aclocal >>./autogen.log 2>&1
-autoconf >>./autogen.log 2>&1
-autoheader >>./autogen.log 2>&1
-automake --add-missing >>./autogen.log 2>&1
+aclocal                | tee -a ./autogen.log 2>&1
+autoconf               | tee -a ./autogen.log 2>&1
+autoheader             | tee -a ./autogen.log 2>&1
+automake --add-missing | tee -a ./autogen.log 2>&1
 
 echo "Completed successfully"
