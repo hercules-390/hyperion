e292f332a579be05a4399ca2e2f69bad12ac2830
Ivan Warren <ivan>
2003-04-18 09:41:18 +0000

    Oopsed in _ macro..
    git-svn-id: file:///home/jj/hercules.svn/trunk@1328 956126f8-22a0-4046-8f4a-272fa8102e63

--- commadpt.c
+++ commadpt.c
@@ -724,7 +724,7 @@ static void commadpt_thread(void *vca)
                     {
                         if(ca->dev->ccwtrace)
                         {
-                            logmsg(_("HHCCA300D %4.4X:Poll Command abort - Poll address >7 Bytes\n",devnum));
+                            logmsg(_("HHCCA300D %4.4X:Poll Command abort - Poll address >7 Bytes\n"),devnum);
                         }
                         ca->badpoll=1;
                         ca->curpending=COMMADPT_PEND_IDLE;
