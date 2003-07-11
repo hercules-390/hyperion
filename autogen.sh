8845d631b66e13fbc2c76239eeaf636806774837
Jay Maynard <jmaynard>
2003-07-11 13:13:22 +0000

    Fixed autogen.sh to invoke libtoolize on OS X correctly.
    git-svn-id: file:///home/jj/hercules.svn/trunk@1650 956126f8-22a0-4046-8f4a-272fa8102e63

--- autogen.sh
+++ autogen.sh
@@ -15,6 +15,32 @@ echo ""
 
 rm -f ./autogen.log
 
+if test ! -e autoconf/ltmain.sh; then
+
+  echo -e "libtoolizing..."
+
+  # Figure out which libtool to use. OS X calls GNU libtool glibtool,
+  #  and has a different program named libtool. We want the GNU version
+  #  here.
+
+  if test -e "`which glibtool`"; then
+    glibtoolize --copy --force --ltdl    >> ./autogen.log 2>&1
+  else
+    libtoolize --copy --force --ltdl     >> ./autogen.log 2>&1
+  fi    
+
+  # Now deal with a minor breakage in GNU libtoolize on OS X: it
+  #  puts the ltmain.sh file in the current directory, not the
+  #  autoconf subdirectory where it belongs. Since other platforms
+  #  probably get this right, we'll only do the move if it exists
+  #  in the current directory.
+
+  if test -e ./ltmain.sh; then
+    mv ltmain.sh autoconf
+  fi
+
+fi
+
 if test ! -e ./ABOUT-NLS; then
 
   echo " Due to a bug(?) in the automake process, if this script"
@@ -22,9 +48,6 @@ if test ! -e ./ABOUT-NLS; then
   echo " you may do that right now if you wish; it's not going to"
   echo " hurt anything if you do."
   echo ""
-
-  echo -e "libtoolizing..."
-  libtoolize --copy --force --ltdl                >> ./autogen.log 2>&1
 
   echo -e "gettextizing..."
   gettextize --copy --force --intl --no-changelog >> ./autogen.log 2>&1
