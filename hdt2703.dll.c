1c7ea95a7eb4c64d7f358d034e3f4c8e64d854d5
Jay Maynard <jmaynard>
2003-08-23 02:22:20 +0000

    Changes required to make "make distcheck" pass again.
    git-svn-id: file:///home/jj/hercules.svn/trunk@1801 956126f8-22a0-4046-8f4a-272fa8102e63

---
+++ hdt2703.dll.c
@@ -0,0 +1,6 @@
+/* This file only exists to make the build process not complain about its */
+/* absence. It should never be referenced by name, and no attempt should  */
+/* ever be made to use it for any reason. If the build process for the    */
+/* dynamic linking process is ever corrected to remove the requirement    */
+/* that this file be present, please remove this file.                    */
+/*                -- Jay Maynard, 22 August 2003                          */
