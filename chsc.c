c4ba267c8e596064c7a7df6f311ea38d8ee0cb94
Jan Jaeger <jj>
2004-11-21 18:22:34 +0000

    Add CHSC commands for Linux/390
    git-svn-id: file:///home/jj/hercules.svn/trunk@2480 956126f8-22a0-4046-8f4a-272fa8102e63

---
+++ chsc.c
@@ -0,0 +1,221 @@
+/* CHSC.C       (c) Copyright Jan Jaeger, 2002-2004                  */
+/*              Channel Subsystem Call                               */
+
+/* Interpretive Execution - (c) Copyright Jan Jaeger, 1999-2004      */
+/* z/Architecture support - (c) Copyright Jan Jaeger, 1999-2004      */
+
+/*-------------------------------------------------------------------*/
+/* This module implements channel subsystem interface functions      */
+/* for the Hercules ESA/390 emulator.                                */
+/*-------------------------------------------------------------------*/
+
+#include "hercules.h"
+
+#include "opcode.h"
+
+#include "inline.h"
+
+#include "chsc.h"
+
+
+#if defined(FEATURE_CHSC)
+
+int ARCH_DEP(chsc_get_sch_desc) (CHSC_REQ *chsc_req, CHSC_RSP *chsc_rsp)
+{
+    U16 req_len, sch, f_sch, l_sch, rsp_len;
+
+    CHSC_REQ4 *chsc_req4 = (CHSC_REQ4 *)(chsc_req);
+    CHSC_RSP4 *chsc_rsp4 = (CHSC_RSP4 *)(chsc_rsp);
+
+    FETCH_HW(f_sch,chsc_req4->f_sch);
+    FETCH_HW(l_sch,chsc_req4->l_sch);
+    
+    /* Fetch length of request field */
+    FETCH_HW(req_len, chsc_req4->length);
+
+    rsp_len = sizeof(CHSC_RSP) + ((1 + l_sch - f_sch) * sizeof(CHSC_RSP4));
+
+    if(l_sch < f_sch 
+      || rsp_len > (0x1000 - req_len)) {
+        /* Set response field length */
+        STORE_HW(chsc_rsp->length,sizeof(CHSC_RSP));
+        /* Store request error */
+        STORE_HW(chsc_rsp->rsp,CHSC_REQ_ERRREQ);
+        /* No reaon code */
+        STORE_FW(chsc_rsp->info,0);
+        return 0;
+    }
+
+    for(sch = f_sch; sch <= l_sch; sch++, chsc_rsp4++)
+    {
+    DEVBLK *dev;
+        memset(chsc_rsp4, 0x00, sizeof(CHSC_RSP4) );
+        if((dev = find_device_by_subchan(sch)))
+        {
+            chsc_rsp4->sch_val = 1;
+            if(dev->pmcw.flag5 & PMCW5_V)
+                chsc_rsp4->dev_val = 1;
+            chsc_rsp4->st = (dev->pmcw.flag25 & PMCW25_TYPE) >> 5;
+            chsc_rsp4->unit_addr = dev->devnum & 0xff;
+            STORE_HW(chsc_rsp4->devno,dev->devnum);
+            chsc_rsp4->path_mask = dev->pmcw.pim;
+            STORE_HW(chsc_rsp4->sch, sch);
+            memcpy(chsc_rsp4->chpid, dev->pmcw.chpid, 8);
+        }
+    }
+    
+    /* Store response length */
+    STORE_HW(chsc_rsp->length,rsp_len);
+    
+    /* Store request OK */
+    STORE_HW(chsc_rsp->rsp,CHSC_REQ_OK);
+
+    /* No reaon code */
+    STORE_FW(chsc_rsp->info,0);
+
+    return 0;
+
+}
+
+
+int ARCH_DEP(chsc_get_css_info) (CHSC_REQ *chsc_req, CHSC_RSP *chsc_rsp)
+{
+CHSC_RSP10 *chsc_rsp10;
+U16 req_len, rsp_len;
+
+    UNREFERENCED(chsc_req);
+
+    chsc_rsp10 = (CHSC_RSP10 *)(chsc_rsp + 1);
+
+    /* Fetch length of request field */
+    FETCH_HW(req_len, chsc_req->length);
+
+    rsp_len = sizeof(CHSC_RSP) + sizeof(CHSC_RSP10);
+
+    if(rsp_len > (0x1000 - req_len)) {
+        /* Set response field length */
+        STORE_HW(chsc_rsp->length,sizeof(CHSC_RSP));
+        /* Store request error */
+        STORE_HW(chsc_rsp->rsp,CHSC_REQ_ERRREQ);
+        /* No reaon code */
+        STORE_FW(chsc_rsp->info,0);
+        return 0;
+    }
+
+    STORE_HW(chsc_rsp->length,rsp_len);
+
+    memset(chsc_rsp10->general_char, 0x00, sizeof(chsc_rsp10->general_char));
+    memset(chsc_rsp10->chsc_char, 0x00, sizeof(chsc_rsp10->chsc_char));
+
+#if defined(FEATURE_QUEUED_DIRECT_IO)
+    chsc_rsp10->general_char[1][1] = 0
+                                   | 0x40  /* Adapter Interruption Facility */
+                                   ;
+
+    chsc_rsp10->chsc_char[3][1] = 0
+                                | 0x10 /* Set Channel Subsystem Char */
+                                | 0x08 /* Fast CHSCs */
+                                ;
+
+    chsc_rsp10->general_char[2][0] = 0
+//                                 | 0x10 /* OSA/FCP Thin interrupts */
+                                   ;
+
+    chsc_rsp10->general_char[1][3] = 0
+//                                 | 0x80 /* AIF Time Delay Disablement Fac */
+                                   ;
+#endif /*defined(FEATURE_QUEUED_DIRECT_IO)*/
+
+    /* Store request OK */
+    STORE_HW(chsc_rsp->rsp,CHSC_REQ_OK);
+
+    /* No reaon code */
+    STORE_FW(chsc_rsp->info,0);
+
+    return 0;
+}
+
+
+/*-------------------------------------------------------------------*/
+/* B25F CHSC  - Channel Subsystem Call                         [RRE] */
+/*-------------------------------------------------------------------*/
+DEF_INST(channel_subsystem_call)
+{
+int     r1, r2;                                 /* register values   */
+VADR    n;                                      /* Unsigned work     */
+BYTE   *mn;                                     /* Unsigned work     */
+U16     req_len;                                /* Length of request */
+U16     req;                                    /* Request code      */
+CHSC_REQ *chsc_req;                             /* Request structure */
+CHSC_RSP *chsc_rsp;                             /* Response structure*/
+
+    RRE(inst, regs, r1, r2);
+
+    PRIV_CHECK(regs);
+
+    SIE_INTERCEPT(regs);
+
+    n = regs->GR(r1) & ADDRESS_MAXWRAP(regs);
+    
+    if(n & 0xFFF)
+        ARCH_DEP(program_interrupt) (regs, PGM_SPECIFICATION_EXCEPTION);
+
+    mn = MADDR(n, r1, regs, ACCTYPE_READ, regs->psw.pkey);
+    chsc_req = (CHSC_REQ*)(mn);
+
+    /* Fetch length of request field */
+    FETCH_HW(req_len, chsc_req->length);
+
+    chsc_rsp = (CHSC_RSP*)((BYTE*)chsc_req + req_len);
+
+    if((req_len < sizeof(CHSC_REQ))
+      || (req_len > (0x1000 - sizeof(CHSC_RSP))))
+        ARCH_DEP(program_interrupt) (regs, PGM_OPERAND_EXCEPTION);
+
+    FETCH_HW(req,chsc_req->req);
+
+    ARCH_DEP(validate_operand) (n, r1, 0, ACCTYPE_WRITE, regs);
+
+    switch(req) {
+
+        case CHSC_REQ_SCHDESC:
+            regs->psw.cc = ARCH_DEP(chsc_get_sch_desc) (chsc_req, chsc_rsp);
+            break;
+
+        case CHSC_REQ_CSSINFO:
+            regs->psw.cc = ARCH_DEP(chsc_get_css_info) (chsc_req, chsc_rsp);
+            break;
+
+        default:
+
+            if( HDC(debug_chsc_unknown_request, chsc_rsp, chsc_req, regs) )
+                break;
+
+            /* Set response field length */
+            STORE_HW(chsc_rsp->length,sizeof(CHSC_RSP));
+            /* Store unsupported command code */
+            STORE_HW(chsc_rsp->rsp,CHSC_REQ_INVALID);
+            /* No reaon code */
+            STORE_FW(chsc_rsp->info,0);
+
+            regs->psw.cc = 0;
+    }
+
+}
+#endif /*defined(FEATURE_CHSC)*/
+
+
+#if !defined(_GEN_ARCH)
+
+#if defined(_ARCHMODE2)
+ #define  _GEN_ARCH _ARCHMODE2
+ #include "chsc.c"
+#endif
+
+#if defined(_ARCHMODE3)
+ #undef   _GEN_ARCH
+ #define  _GEN_ARCH _ARCHMODE3
+ #include "chsc.c"
+#endif
+
+#endif /*!defined(_GEN_ARCH)*/
---
+++ chsc.h
@@ -0,0 +1,74 @@
+/* CHSC.H       (c) Copyright Jan Jaeger, 1999-2004                  */
+/*              Channel Subsystem interface fields                   */
+
+
+#if !defined(_CHSC_H)
+
+#define _CHSC_H
+
+// #if defined(FEATURE_CHSC)
+typedef struct _CHSC_REQ {
+        HWORD   length;                 /* Offset to response field  */
+        HWORD   req;                    /* Request code              */
+        FWORD   resv[3];
+    } CHSC_REQ;
+
+typedef struct _CHSC_REQ4 {
+        HWORD   length;                 /* Offset to response field  */
+        HWORD   req;                    /* Request code              */
+#define CHSC_REQ_SCHDESC        0x04
+#define CHSC_REQ_CSSINFO        0x10
+        HWORD   resv1;  
+        HWORD   f_sch;                  /* First subchannel          */
+        HWORD   resv2;
+        HWORD   l_sch;                  /* Last subchannel           */
+        FWORD   resv3;
+    } CHSC_REQ4;
+
+typedef struct _CHSC_RSP {
+        HWORD   length;                 /* Length of response field  */
+        HWORD   rsp;                    /* Reponse code              */
+#define CHSC_REQ_OK             0x0001  /* No error                  */
+#define CHSC_REQ_INVALID        0x0002  /* Invalid request           */
+#define CHSC_REQ_ERRREQ         0x0003  /* Error in request block    */
+#define CHSC_REQ_NOTSUPP        0x0004  /* Request not supported     */
+        FWORD   info;
+    } CHSC_RSP;
+
+typedef struct _CHSC_RSP4 {
+        BYTE    sch_val : 1;            /* Subchannel valid          */
+        BYTE    dev_val : 1;            /* Device number valid       */
+        BYTE    st : 3;                 /* Subchannel type           */
+#define CHSC_RSP4_ST_IO     0           /* I/O Subchannel; all fields
+                                           have a meaning            */
+#define CHSC_RSP4_ST_CHSC   1           /* CHSC Subchannel only sch_val
+                                           st and sch have a meaning */
+#define CHSC_RSP4_ST_MSG    2           /* MSG Subchannel; all fields
+                                           except unit_addr have a 
+                                           meaning                   */
+#define CHSC_RPS4_ST_ADM    3           /* ADM Subchannel; Only sch_val
+                                           st and sch have a meaning */
+        BYTE    zeros : 3;
+        BYTE    unit_addr;              /* Unit address              */
+        HWORD   devno;                  /* Device number             */
+        BYTE    path_mask;              /* Valid path mask           */
+        BYTE    fla_valid_mask;         /* Valid link mask           */
+        HWORD   sch;                    /* Subchannel number         */
+        BYTE    chpid[8];               /* Channel path array        */
+        BYTE    fla[8];                 /* Full link address array   */
+    } CHSC_RSP4;
+
+typedef struct _CHSC_RSP10 {
+        FWORD   general_char[510];
+        FWORD   chsc_char[508];         /* ZZ: Linux/390 code indicates 
+                                           this field has a length of
+                                           518, however, that would 
+                                           mean that the entire CHSC
+                                           request would be 4K + 16
+                                           in length which is probably
+                                           an error -    *JJ/10/10/04*/
+    } CHSC_RSP10;
+
+// #endif /*defined(FEATURE_CHSC)*/
+
+#endif /*!defined(_CHSC_H)*/
