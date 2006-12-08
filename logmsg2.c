37edc4cf5aefa38a0bb2fddec89d45480efa3a7a
Jan Jaeger <jj>
2006-12-08 09:47:49 +0000

    Delete obsolete files
    git-svn-id: file:///home/jj/hercules.svn/trunk@4103 956126f8-22a0-4046-8f4a-272fa8102e63

--- logmsg2.c
+++
@@ -1,5 +0,0 @@
-/* LOGMSG2.C    (c) Copyright Roger Bowler, 2005-2006                */
-/*              Message log function for batch programs              */
-
-/* This module is no longer used                                     */
-
