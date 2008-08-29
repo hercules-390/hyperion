3d5943ec72411ae6d52bcc0806209cc71a50e24b
Fish (David B Trout) <fish>
2008-08-29 07:07:08 +0000

    remove unnecessary blank line following directory report
    git-svn-id: file:///home/jj/hercules.svn/trunk@4857 956126f8-22a0-4046-8f4a-272fa8102e63

--- fillfnam.c
+++ fillfnam.c
@@ -1,6 +1,9 @@
 // $Id$
 //
 // $Log$
+// Revision 1.11  2008/02/18 22:57:08  rbowler
+// Disable filename completion for Solaris 2.9 due to missing scandir function
+//
 // Revision 1.10  2006/12/31 17:52:50  gsmith
 // 31 Dec 2006 Fix typo in line 1 of fillfnam.c
 //
@@ -196,7 +199,6 @@ int tab_pressed(char *cmdlinefull, int *cmdoffset) {
       /* display all alternatives */
       for (i = 0; i< n; i++)
          logmsg("%s\n", namelist[i]->d_name);
-      logmsg("\n");
     }
     /* free everything */
     free(buff);
