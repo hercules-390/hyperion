8a1c4d7dcb45b70ac4cacf0f1e7ee681c8184ee4
Roger Bowler <rbowler>
2009-06-16 14:53:27 +0000

    Additional 370 optional extra features
    git-svn-id: file:///home/jj/hercules.svn/trunk@5414 956126f8-22a0-4046-8f4a-272fa8102e63

--- feat370.h
+++ feat370.h
@@ -2,44 +2,6 @@
 /*              S/370 feature definitions                            */
 
 // $Id$
-//
-// $Log$
-// Revision 1.31  2008/09/18 11:12:58  rbowler
-// Permit General Instructions Extension facility to be activated in S/370 mode
-//
-// Revision 1.30  2008/01/21 22:20:35  rbowler
-// Correct previous changelog entry (ETF3 not ETF2)
-//
-// Revision 1.29  2008/01/21 14:02:13  rbowler
-// Permit Extended-Translation-Facility-3 in S/370 and ESA/390 modes
-//
-// Revision 1.28  2007/12/02 16:34:23  rbowler
-// Permit Extended-Translation-Facility-2 to be activated in S/370 mode
-//
-// Revision 1.27  2007/12/02 15:45:17  rbowler
-// Permit Extended-Translation facility to be activated in S/370 mode
-//
-// Revision 1.26  2007/12/02 15:32:46  rbowler
-// Permit Compare-and-Move-Extended facility to be activated in S/370 mode
-//
-// Revision 1.25  2007/11/30 15:14:14  rbowler
-// Permit String-Instruction facility to be activated in S/370 mode
-//
-// Revision 1.24  2007/11/18 22:18:51  rbowler
-// Permit FEATURE_IMMEDIATE_AND_RELATIVE to be activated in S/370 mode
-//
-// Revision 1.23  2007/06/23 00:04:09  ivan
-// Update copyright notices to include current year (2007)
-//
-// Revision 1.22  2006/12/31 17:53:48  gsmith
-// 2006 Dec 31 Update ecpsvm.c for new psw IA scheme
-//
-// Revision 1.21  2006/12/20 04:26:19  gsmith
-// 19 Dec 2006 ip_all.pat - performance patch - Greg Smith
-//
-// Revision 1.20  2006/12/08 09:43:21  jj
-// Add CVS message log
-//
 
 #if defined(OPTION_370_MODE)
 #define _ARCH_370_NAME "S/370"
@@ -66,20 +28,30 @@
 /* The following ESA/390 features can be retrofitted to S/370 and
    may be activated if desired by uncommenting the appropriate
    define statements below and performing a complete rebuild */
-//#define FEATURE_IMMEDIATE_AND_RELATIVE
-//#define FEATURE_STRING_INSTRUCTION
+//#define FEATURE_BASIC_FP_EXTENSIONS
+//#define FEATURE_BINARY_FLOATING_POINT
+//#define FEATURE_CHECKSUM_INSTRUCTION
 //#define FEATURE_COMPARE_AND_MOVE_EXTENDED
+//#define FEATURE_COMPRESSION
 //#define FEATURE_EXTENDED_TRANSLATION
 //#define FEATURE_EXTENDED_TRANSLATION_FACILITY_2
+//#define FEATURE_HFP_EXTENSIONS
+//#define FEATURE_HFP_MULTIPLY_ADD_SUBTRACT
+//#define FEATURE_IMMEDIATE_AND_RELATIVE
+//#define FEATURE_SQUARE_ROOT
+//#define FEATURE_STRING_INSTRUCTION
 
 /* The following ESAME features can be retrofitted to S/370 and
    may be activated if desired by uncommenting the appropriate
    define statements below and performing a complete rebuild */
-//#define FEATURE_EXTENDED_TRANSLATION_FACILITY_3
+//#define FEATURE_ESAME_N3_ESA390
 //#define FEATURE_ETF2_ENHANCEMENT
 //#define FEATURE_ETF3_ENHANCEMENT
-//#define FEATURE_LONG_DISPLACEMENT
+//#define FEATURE_EXECUTE_EXTENSIONS_FACILITY
+//#define FEATURE_EXTENDED_IMMEDIATE
+//#define FEATURE_EXTENDED_TRANSLATION_FACILITY_3
 //#define FEATURE_GENERAL_INSTRUCTIONS_EXTENSION_FACILITY
+//#define FEATURE_LONG_DISPLACEMENT
 //#define FEATURE_MESSAGE_SECURITY_ASSIST
 //#define FEATURE_MESSAGE_SECURITY_ASSIST_EXTENSION_1
 //#define FEATURE_MESSAGE_SECURITY_ASSIST_EXTENSION_2
