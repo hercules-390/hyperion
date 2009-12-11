9a91742b55f8235daef4e92f3be7a9830fb70508
Bernard van der Helm <bernard>
2009-12-11 09:54:30 +0000

    Killing the array subscript warnings
    git-svn-id: file:///home/jj/hercules.svn/trunk@5508 956126f8-22a0-4046-8f4a-272fa8102e63

--- hconsts.h
+++ hconsts.h
@@ -268,12 +268,18 @@
 #define ACCTYPE_STRAG      0               /* STRAG instruction      */
 #define ACCTYPE_LPTEA     (ACC_LPTEA|ACC_NOTLB) /* LPTEA instruction */
 
-/* Special value for arn parameter for translate functions in dat.c */
+/* Special value for arn parameter for translate functions in dat.c  */
+/* _USE_... values for killing the "array subscript" warnings        */
 #define USE_INST_SPACE          (-1)    /* Instruction space virtual */
+#define _USE_INST_SPACE         4
 #define USE_REAL_ADDR           (-2)    /* Real address              */
+#define _USE_REAL_ADDR          3
 #define USE_PRIMARY_SPACE       (-3)    /* Primary space virtual     */
+#define _USE_PRIMARY_SPACE      2
 #define USE_SECONDARY_SPACE     (-4)    /* Secondary space virtual   */
+#define _USE_SECONDARY_SPACE    1
 #define USE_HOME_SPACE          (-5)    /* Home space virtual        */
+#define _USE_HOME_SPACE         0
 #define USE_ARMODE              16      /* OR with access register
                                            number to force AR mode   */
 
