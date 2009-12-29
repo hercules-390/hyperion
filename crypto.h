366e4225b4ab5f19a552c0e1224806770cc31fa7
Bernard van der Helm <bernard>
2009-12-29 13:13:15 +0000

    Simplification dyncrypt.c (reduction of 1250 lines)
    git-svn-id: file:///home/jj/hercules.svn/trunk@5572 956126f8-22a0-4046-8f4a-272fa8102e63

--- crypto.h
+++ crypto.h
@@ -25,8 +25,7 @@
 
 CRY_DLL_IMPORT void ( ATTR_REGPARM(2) *ARCH_DEP( cipher_message                      ) ) ( BYTE*, REGS* );
 CRY_DLL_IMPORT void ( ATTR_REGPARM(2) *ARCH_DEP( cipher_message_with_chaining        ) ) ( BYTE*, REGS* );
-CRY_DLL_IMPORT void ( ATTR_REGPARM(2) *ARCH_DEP( compute_intermediate_message_digest ) ) ( BYTE*, REGS* );
-CRY_DLL_IMPORT void ( ATTR_REGPARM(2) *ARCH_DEP( compute_last_message_digest         ) ) ( BYTE*, REGS* );
+CRY_DLL_IMPORT void ( ATTR_REGPARM(2) *ARCH_DEP( compute_message_digest              ) ) ( BYTE*, REGS* );
 CRY_DLL_IMPORT void ( ATTR_REGPARM(2) *ARCH_DEP( compute_message_authentication_code ) ) ( BYTE*, REGS* );
 
 #endif /*defined(_FEATURE_MESSAGE_SECURITY_ASSIST)*/
