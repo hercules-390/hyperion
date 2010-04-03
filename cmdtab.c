f9fa374c0ce55010c4edd99da9499438120e241d
Paul Gorlinsky <pgorlinsky>
2010-04-03 14:10:49 +0000

    Removed echoing of input for scp and priority scp commands. OS does it. Removes a security issue regarding display of passwords.
    git-svn-id: file:///home/jj/hercules.svn/trunk@5790 956126f8-22a0-4046-8f4a-272fa8102e63

--- cmdtab.c
+++ cmdtab.c
@@ -477,63 +477,61 @@ void *panel_command (void *cmdline)
     if (!sysblk.inststep && (sysblk.cmdtgt == 0) && (0 == cmd[0]))
         return NULL;
 
-    /* Echo the command to the control panel */
-    if(!noredisp)
-    {
-        logmsg( "%s\n", cmd);
-    }
-
 #ifdef OPTION_CMDTGT
     /* check for herc, scp or pscp command */
     /* Please note that cmdtgt is a hercules command! */
     /* Changing the target to her in scp mode is done by using herc cmdtgt herc */
     if(!strncasecmp(cmd, "herc ", 5) || !strncasecmp(cmd, "scp ", 4) || !strncasecmp(cmd, "pscp ", 5))
     {
-      ProcessPanelCommand(cmd);
-      return NULL;
+        if (!noredisp) logmsg("%s\n", cmd);     // Echo command to the control panel
+        ProcessPanelCommand(cmd);
+        return NULL;
     }
 
     /* Send command to the selected command target */
     switch(sysblk.cmdtgt)
     {
-      case 0: // cmdtgt herc
-      {
+        case 0: // cmdtgt herc
+        {
         /* Stay compatible */
 #ifdef _FEATURE_SYSTEM_CONSOLE
-        if(cmd[0] == '.' || cmd[0] == '!')
-        {
-          if(!cmd[1])
-          {
-            cmd[1] = ' ';
-            cmd[2] = 0;
-          }
-          scp_command(cmd + 1, cmd[0] == '!');
-        }
-        else
+            if(cmd[0] == '.' || cmd[0] == '!')
+            {
+                if(!cmd[1])
+                {
+                    cmd[1] = ' ';
+                    cmd[2] = 0;
+                }
+            scp_command(cmd + 1, cmd[0] == '!');
+            }
+            else
 #endif /*_FEATURE_SYSTEM_CONSOLE*/
-          ProcessPanelCommand(cmd);
-        break;
-      }
-      case 1: // cmdtgt scp
-      {
-        if(!cmd[0])
-        {
-          cmd[0] = ' ';
-          cmd[1] = 0;
+            {
+                if (!noredisp) logmsg("%s\n", cmd);     // Echo command to the control panel
+                ProcessPanelCommand(cmd);
+            }
+            break;
         }
-        scp_command(cmd, 0);
-        break;
-      }
-      case 2: // cmdtgt pscp
-      {
-        if(!cmd[0])
+        case 1: // cmdtgt scp
         {
-          cmd[0] = ' ';
-          cmd[1] = 0;
+            if(!cmd[0])
+            {
+                cmd[0] = ' ';
+                cmd[1] = 0;
+            }
+            scp_command(cmd, 0);
+            break;
         }
-        scp_command(cmd, 1);
-        break;
-      }
+        case 2: // cmdtgt pscp
+        {
+            if(!cmd[0])
+            {
+                cmd[0] = ' ';
+                cmd[1] = 0;
+            }
+            scp_command(cmd, 1);
+            break;
+        }
     }
 #else // OPTION_CMDTGT
 #ifdef _FEATURE_SYSTEM_CONSOLE
@@ -544,7 +542,7 @@ void *panel_command (void *cmdline)
         return NULL;
     }
 #endif /*_FEATURE_SYSTEM_CONSOLE*/
-
+    if (!noredisp) logmsg("%s\n", cmd);     // Echo command to the control panel
     ProcessPanelCommand(cmd);
 #endif // OPTION_CMDTGT
 
