15b24c05f60517e158ac2b41f05d031e51e60525
Paul Gorlinsky <pgorlinsky>
2010-06-16 15:00:57 +0000

    Rework quit messages.
    git-svn-id: file:///home/jj/hercules.svn/trunk@6129 956126f8-22a0-4046-8f4a-272fa8102e63

--- msgenu.h
+++ msgenu.h
@@ -115,7 +115,7 @@ cpu.c:123:HABC1234I This is a message
 
 // reserve 43-58 for option related
 
-#define HHC00069 "System is not quiesced; there %s %d CPU%s active"
+#define HHC00069 "There %s %d CPU%s still active; confirmation required"
 // HHC0007xx, HHC0008xx and HHC0009xx reserved for hao.c. (to recognize own messages)
 #define HHC00070 "Unknown hao command, valid commands are:\n" \
        "          hao tgt <tgt> : define target rule (pattern) to react on\n" \
@@ -1042,7 +1042,7 @@ cpu.c:123:HABC1234I This is a message
 #define HHC02263 "Resuming script file processing..."
 #define HHC02264 "Script file processing complete"
 #define HHC02265 "Script file '%s' aborted due to previous conditions"
-#define HHC02266 "Reenter command '%s' again within %d seconds to execute"
+#define HHC02266 "Confirm command by entering '%s' again within %d seconds"
 //efine HHC02267 "%s" /* Used for instruction tracing */
 //efine HHC02268 "%s" /* Used for ds command */
 #define HHC02269 "%s" 
