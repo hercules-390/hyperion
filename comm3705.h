b4ebcdfc9925634527055661cc1c5335cbcd9dff
Roger Bowler <rbowler>
2010-07-29 09:02:50 +0000

    Fix svn id and line termination properties
    git-svn-id: file:///home/jj/hercules.svn/trunk@6239 956126f8-22a0-4046-8f4a-272fa8102e63

--- comm3705.h
+++ comm3705.h
@@ -6,7 +6,7 @@
 /*   (http://www.hercules-390.org/herclic.html) as modifications to  */
 /*   Hercules.                                                       */
 
-// $Id: comm3705.h 5925 2010-05-03 14:34:25Z bernard $
+// $Id$
 
 #ifndef __COMM3705_H__
 #define __COMM3705_H__
