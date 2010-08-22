60f93820dd5c751dcd91839dfd9fb3d81ae5543f
Roger Bowler <rbowler>
2010-08-22 14:48:31 +0000

    warning: 'result' 'cc' may be used uninitialized
    git-svn-id: file:///home/jj/hercules.svn/trunk@6339 956126f8-22a0-4046-8f4a-272fa8102e63

--- general3.c
+++ general3.c
@@ -2487,6 +2487,9 @@ BYTE    opcode;                         /* 2nd byte of opcode        */
             /* Add unsigned operands and set condition code */
             cc = add_logical (&result, v2, v3);
             break;
+        default: /* To prevent compiler warnings */
+            result = 0;
+            cc = 0;
         } /* end switch(opcode) */
 
         /* Interlocked exchange to storage location */
@@ -2560,6 +2563,9 @@ BYTE    opcode;                         /* 2nd byte of opcode        */
             /* Add unsigned operands and set condition code */
             cc = add_logical_long (&result, v2, v3);
             break;
+        default: /* To prevent compiler warnings */
+            result = 0;
+            cc = 0;
         } /* end switch(opcode) */
 
         /* Interlocked exchange to storage location */
