7af9a3db6251d2e60a70ded25d563d8ea9dde064
Bernard van der Helm <bernard>
2010-09-17 04:55:03 +0000

    PCKMO fixes
    git-svn-id: file:///home/jj/hercules.svn/trunk@6507 956126f8-22a0-4046-8f4a-272fa8102e63

--- crypto/dyncrypt.c
+++ crypto/dyncrypt.c
@@ -3723,12 +3723,12 @@ DEF_INST(perform_cryptographic_key_management_operations_d)
       if(fc <= 3)
       {
         dea_wrap(parameter_block, keylen);
-        memcpy(&parameter_block[parameter_blocklen], wkvp_regs_dea, 24);
+        memcpy(&parameter_block[keylen], wkvp_regs_dea, 24);
       }
       else
       {
         aes_wrap(parameter_block, keylen);
-        memcpy(&parameter_block[parameter_blocklen], wkvp_regs_aes, 32);
+        memcpy(&parameter_block[keylen], wkvp_regs_aes, 32);
       }
         
       /* Store the parameterblock */
