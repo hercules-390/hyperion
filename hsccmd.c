26ee588072aa86c3677918291b6c4ef54ec4a746
Jan Jaeger <jj>
2010-09-17 17:28:43 +0000

    git-svn-id: file:///home/jj/hercules.svn/trunk@6524 956126f8-22a0-4046-8f4a-272fa8102e63

--- hsccmd.c
+++ hsccmd.c
@@ -5550,8 +5550,6 @@ int attach_cmd(int argc, char *argv[], char *cmdline)
         return -1;
     }
     rc = parse_and_attach_devices(argv[1],argv[2],argc-3,&argv[3]);
-    if( rc == 0 && MLVL(VERBOSE) )
-        WRMSG(HHC02198, "I");
 
     return rc;
 }
