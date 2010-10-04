7d21afb25580eb9f42cbfcdf68f650d1b8f4cb75
Jan Jaeger <jj>
2010-10-04 20:48:30 +0000

    Add herror.h
    git-svn-id: file:///home/jj/hercules.svn/trunk@6787 956126f8-22a0-4046-8f4a-272fa8102e63

---
+++ herror.h
@@ -0,0 +1,25 @@
+/* HERROR.H     (c) Copyright Jan Jaeger, 2010                       */
+/*              Hercules Specfic Error codes                         */
+/*                                                                   */
+/*   Released under "The Q Public License Version 1"                 */
+/*   (http://www.hercules-390.org/herclic.html) as modifications to  */
+/*   Hercules.                                                       */
+
+// $Id$
+
+
+#ifndef _HERROR_H
+#define _HERROR_H
+
+/* Generic codes */
+#define HNOERROR       (0)                          /* OK / NO ERROR */
+#define HERROR        (-1)                          /* Generic Error */
+
+/* CPU related error codes */
+#define HERRCPUOFF    (-2)                            /* CPU Offline */
+
+/* Device related error codes */
+#define HERRDEVIDA    (-2)                 /* Invalid Device Address */
+
+
+#endif /* _HERROR_H */
