9b649480ffb42e8575350f4ab2852c5b499701ac
Jan Jaeger <jj>
2010-10-15 20:58:45 +0000

    git-svn-id: file:///home/jj/hercules.svn/trunk@6914 956126f8-22a0-4046-8f4a-272fa8102e63

--- msgenu.h
+++ msgenu.h
@@ -94,10 +94,18 @@ cpu.c:123:HABC1234I This is a message
 
 #define MSG(id, s, ...)              #id s " " id "\n", ## __VA_ARGS__
 #define MSG_C(id, s, ...)            #id s " " id "", ## __VA_ARGS__
+#if 1 // ZZ
 #define WRMSG(id, s, ...)            writemsg(__FILE__, __LINE__, __FUNCTION__, 0, MLVL(ANY), "", _(#id s " " id "\n"), ## __VA_ARGS__)
 #define WRMSG_C(id, s, ...)          writemsg(__FILE__, __LINE__, __FUNCTION__, 0, MLVL(ANY), "", _(#id s " " id ""), ## __VA_ARGS__)
 #define WRCMSG(color, id, s, ...)    writemsg(__FILE__, __LINE__, __FUNCTION__, 0, MLVL(ANY), color, _(#id s " " id "\n"), ## __VA_ARGS__)
 #define WRCMSG_C(color, id, s, ...)  writemsg(__FILE__, __LINE__, __FUNCTION__, 0, MLVL(ANY), color, _(#id s " " id ""), ## __VA_ARGS__)
+#else
+/* simple message processing */
+#define WRMSG(id, s, ...) printf(#id s " " id "\n", ## __VA_ARGS__)
+#define WRMSG_C(id, s, ...) printf(#id s " " id, ## __VA_ARGS__)
+#define WRCMSG(c, id, s, ...) printf(#id s " " id "\n", ## __VA_ARGS__)
+#define WRCMSG_C(c, id, s, ...) printf(#id s " " id, ## __VA_ARGS__)
+#endif
 
 #ifndef OPTION_MSGLCK
 #define WRGMSG_ON \
