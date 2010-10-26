1f0814cdbed61cbf797735c24fd917220e5d2c0c
Paul Gorlinsky <pgorlinsky>
2010-10-26 17:23:22 +0000

    Fix ASSERT macro use.
    git-svn-id: file:///home/jj/hercules.svn/trunk@6984 956126f8-22a0-4046-8f4a-272fa8102e63

--- msgenu.h
+++ msgenu.h
@@ -101,7 +101,8 @@ cpu.c:123:HABC1234I This is a message
          char _msgbuf[32768]; \
          int _rc; \
          _rc = MSGBUF( _msgbuf, #id s " " id "\n", ## __VA_ARGS__); \
-         ASSERT( _rc != -1 || _rc < (int)sizeof(_msgbuf)-1 ); \
+         ASSERT( _rc != -1 ); \
+         ASSERT( _rc < (int)sizeof(_msgbuf)-1 ); \
          writemsg(__FILE__, __LINE__, __FUNCTION__, 0, MLVL(ANY), "", "%s", _msgbuf ); \
        } while(0)
 
@@ -110,7 +111,8 @@ cpu.c:123:HABC1234I This is a message
          char _msgbuf[32768]; \
          int _rc; \
          _rc = MSGBUF( _msgbuf, #id s " " id "", ## __VA_ARGS__); \
-         ASSERT( _rc != -1 || _rc < (int)sizeof(_msgbuf)-1 ); \
+         ASSERT( _rc != -1 ); \
+         ASSERT( _rc < (int)sizeof(_msgbuf)-1 ); \
          writemsg(__FILE__, __LINE__, __FUNCTION__, 0, MLVL(ANY), "", "%s", _msgbuf ); \
        } while(0)
 
@@ -119,7 +121,8 @@ cpu.c:123:HABC1234I This is a message
          char _msgbuf[32768]; \
          int _rc; \
          _rc = MSGBUF( _msgbuf, #id s " " id "\n", ## __VA_ARGS__); \
-         ASSERT( _rc != -1 || _rc < (int)sizeof(_msgbuf)-1 ); \
+         ASSERT( _rc != -1 ); \
+         ASSERT( _rc < (int)sizeof(_msgbuf)-1 ); \
          writemsg(__FILE__, __LINE__, __FUNCTION__, 0, MLVL(ANY), color, "%s", _msgbuf ); \
        } while(0)
 
@@ -128,7 +131,8 @@ cpu.c:123:HABC1234I This is a message
          char _msgbuf[32768]; \
          int _rc; \
          _rc = MSGBUF( _msgbuf, #id s " " id "\n", ## __VA_ARGS__); \
-         ASSERT( _rc != -1 || _rc < (int)sizeof(_msgbuf)-1 ); \
+         ASSERT( _rc != -1 ); \
+         ASSERT( _rc < (int)sizeof(_msgbuf)-1 ); \
          writemsg(__FILE__, __LINE__, __FUNCTION__, 0, MLVL(ANY), color, "%s", _msgbuf ); \
        } while(0)
 #else
