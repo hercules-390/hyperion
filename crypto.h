d1eb244b5b496c48b53ca2da937c072b63834dd3
Bernard van der Helm <bernard>
2010-11-02 20:26:19 +0000

    Remove crypto.h file
    git-svn-id: file:///home/jj/hercules.svn/trunk@7042 956126f8-22a0-4046-8f4a-272fa8102e63

--- crypto.h
+++
@@ -1,9 +0,0 @@
-/* CRYPTO.H     (c) Copyright Jan Jaeger, 2000-2010                  */
-/*              Cryptographic instructions                           */
-/*                                                                   */
-/*   Released under "The Q Public License Version 1"                 */
-/*   (http://www.hercules-390.org/herclic.html) as modifications to  */
-/*   Hercules.                                                       */
-
-// $Id$
-
