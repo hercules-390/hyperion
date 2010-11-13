57eb394866f74d80fa266a4631ce00b415ff24f8
Paul Gorlinsky <pgorlinsky>
2010-11-13 03:26:36 +0000

    Extra ';' removed in macros
    git-svn-id: file:///home/jj/hercules.svn/trunk@7105 956126f8-22a0-4046-8f4a-272fa8102e63

--- hthreads.h
+++ hthreads.h
@@ -186,7 +186,7 @@ typedef fthread_attr_t    ATTR;
 #define detach_thread(tid)                     fthread_detach((tid))
 #define signal_thread(tid,signo)               fthread_kill((tid),(signo))
 #define thread_id()                            fthread_self()
-#define win_thread_handle(tid)                 fthread_get_handle(tid);
+#define win_thread_handle(tid)                 fthread_get_handle(tid)
 #define exit_thread(exitvar_ptr)               fthread_exit((exitvar_ptr))
 #define equal_threads(tid1,tid2)               fthread_equal((tid1),(tid2))
 
