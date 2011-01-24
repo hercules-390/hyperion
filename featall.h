fe101102101fb2fe87645e81ba8344279a4690c6
Bernard van der Helm <bernard>
2011-01-24 17:33:41 +0000

    OPTION_OPTINST will be re-assessed
    git-svn-id: file:///home/jj/hercules.svn/trunk@7297 956126f8-22a0-4046-8f4a-272fa8102e63

--- featall.h
+++ featall.h
@@ -71,7 +71,7 @@
 //                                         locked during execution   */
 #define OPTION_ENHANCED_DEVICE_ATTACH   /* Multiple device att feat  */
 //#define  OPTION_BUILTIN_SYMBOLS       /* Internal Symbols Defined  */
-#define OPTION_OPTINST                  /* Optimized instructions    */
+#undef  OPTION_OPTINST                  /* Optimized instructions    */
 
 #if defined(OPTION_BUILTIN_SYMBOLS) && !defined(OPTION_CONFIG_SYMBOLS)
   #error OPTION_BUILTIN_SYMBOLS requires OPTION_CONFIG_SYMBOLS
