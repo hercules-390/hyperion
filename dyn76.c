347d0be19c8ee072842d17aed4a6ad468e92eb78
Harold Grovesteen <hsg001>
2011-03-19 15:07:17 +0000

    DIAGNOSE X'F18' - improves restart state handling
    git-svn-id: file:///home/jj/hercules.svn/trunk@7361 956126f8-22a0-4046-8f4a-272fa8102e63

--- dyn76.c
+++ dyn76.c
@@ -58,8 +58,11 @@ struct fkeeper
     char filename [260];  /* Also used for 256 byte read/write buffer */
 };
 
-/* File Keeper list.  New entries are added at the top of the list */
-static struct fkeeper *fkpr_head = NULL;  /* open file status list */
+/* File Keeper lists.  New entries are added at the top of the list
+   Because both lists are treated a single shared resource they utilize
+   the same lock, nfile_lock, to control CPU (thread) access */
+
+static struct fkeeper *fkpr_head = NULL;  /* Open file status list */
 static struct fkeeper *rst_head = NULL;   /* Restart list */
 
 static unsigned char DCCascii_to_ebcdic[] = 
@@ -138,10 +141,13 @@ static void MemConverter (char * s, unsigned char * cct, int len)
 
 #endif
 
+/* File keeper list controls */
 static LOCK nfile_lock;
 static long nfile_init_req = 1;
 static U32 nfile_id = 0;
 static U32 restart_id = 0;
+#define DO_FREE 1
+#define NO_FREE 0
 
 static void nfile_init () 
 {
@@ -174,57 +180,37 @@ static void nfile_init ()
 #define _O_EXCL   O_EXCL
 #endif
 
-#if 0
-static int GetMode (int h) 
+/* WARNING: This function MUST be called with nfile_lock already locked */
+static void AddFKByID (U32 id, struct fkeeper *item, struct fkeeper **list)
 {
-    int mode = 0;
-    struct fkeeper *fk;
-
-    dolock (nfile_lock); /* Take ownership of the list */
-    fk = fkpr_head; /* Search the list */
-    while (fk) 
-    {
-        if (fk->handle == h) 
-        {   /* Found the entry? */
-            mode = fk->mode;
-            break;
-        }
-        fk = fk->next;
-    }
-    unlock (nfile_lock); /* Release ownership of the list */
-    return (mode);
+    /* struct fkeeper * head = list; */
+    item->id = id;
+    item->next = *list;
+    *list = item;
+#ifdef DYN76_DEBUG
+    LOGMSG("DF18: AddFKByID added id=%d at %X with next item at %X to list head=%X at %X\n", 
+           item->id, item, item->next, *list, list);
+#endif 
 }
 
-static int SetMode (int h, int m) 
-{
-    int mode = 0;
-    struct fkeeper *fk;
-
-    dolock (nfile_lock); /* Take ownership of the list */
-    fk = fkpr_head; /* Search the list */
-    while (fk) 
-    {
-        if (fk->handle == h) 
-        {   /* Found the entry? */
-            mode = fk->mode;
-            fk->mode = m;
-            break;
-        }
-        fk = fk->next;
-    }
-    unlock (nfile_lock); /* Release ownership of the list */
-    return (mode);
-}
-# endif
-
-static struct fkeeper * FindFK (U32 id)
+static struct fkeeper * FindFK (U32 id, struct fkeeper **list)
 {
     struct fkeeper *fk;
-
+#ifdef DYN76_DEBUG
+    LOGMSG("DF18: FindFK id=%d in list head at=%X\n", id, list);
+#endif
     dolock (nfile_lock); /* Take ownership of the list */
-    fk = fkpr_head;      /* Search the list */
+
+    fk = *list;          /* Search the list */
+
+#ifdef DYN76_DEBUG
+    LOGMSG("DF18: FindFK list head points to first entry at %X\n", fk);
+#endif
     while (fk) 
     {
+#ifdef DYN76_DEBUG
+        LOGMSG("DF18: FindFK id=%d (0x%X) is at: %X\n", fk->id, fk->id, fk);
+#endif
         if (fk->id == id) 
         {   /* Found the entry */
             unlock (nfile_lock);
@@ -237,51 +223,38 @@ static struct fkeeper * FindFK (U32 id)
     return 0;
 }
 
-#if 0
-static void RemoveFK (int h) 
+static void RemoveFKByID (U32 id, struct fkeeper **list, int free_entry) 
 {
     struct fkeeper *pfk;
     struct fkeeper *fk;
-
-    dolock (nfile_lock); /* Take ownership of the list */
-    pfk = NULL;          /* previous entry */
-    fk = fkpr_head;      /* Current entry, starting with the head */
-    while (fk) 
-    {
-        if (fk->handle == h) 
-        {   /* Found the entry? */
-            if (pfk) /* Wasn't the head entry */
-                pfk->next = fk->next;
-            else     /* Was the head entry */
-                fkpr_head = fk->next;
-            free (fk);
-            break;
-        }
-        /* Set up for next cycle */
-        pfk = fk;      /* current entry becomes the previous */
-        fk = fk->next; /* current entry's next, becomes the current */
-    }
-    unlock (nfile_lock); /* Release ownership of the list */
-}
+#ifdef DYN76_DEBUG
+    LOGMSG("DF18: RemoveFKByID id %d from list head at=%X\n", id, list);
 #endif
-
-static void RemoveFKByID (U32 id) 
-{
-    struct fkeeper *pfk;
-    struct fkeeper *fk;
-
     dolock (nfile_lock); /* Take ownership of the list */
     pfk = NULL;
-    fk = fkpr_head; /* Search the list */
+    fk = *list;
     while (fk) 
     {
+#ifdef DYN76_DEBUG
+        LOGMSG("DF18: RemoveFKByID id=%d (0x%X) is at: %X\n", fk->id, fk->id, fk);
+#endif
         if (fk->id == id) 
         {   /* Found the entry */
             if (pfk) /* Wasn't the head entry */
+            {
                 pfk->next = fk->next;
+            }
             else     /* Was the head entry */
-                fkpr_head = fk->next;
-            free (fk);
+            {
+                *list = fk->next;
+            }
+            if (free_entry)
+            {
+#ifdef DYN76_DEBUG
+                LOGMSG("DF18: RemoveFKByID freeing id=%d at: %X\n", fk->id, fk);
+#endif
+                free (fk);
+            }
             break;
         }
         pfk = fk;
@@ -290,7 +263,8 @@ static void RemoveFKByID (U32 id)
     unlock (nfile_lock); /* Release ownership of the list */
 }
 
-static int RemoveFKByName (char * filename) {
+static int RemoveFKByName (char * filename) 
+{
     int i = -1 * EBADF;
     struct fkeeper *pfk;
     struct fkeeper *fk;
@@ -307,9 +281,13 @@ static int RemoveFKByName (char * filename) {
                 i = -errno;
             else {
                 if (pfk) /* Wasn't the head entry */
+                {
                     pfk->next = fk->next;
+                }
                 else     /* Was the head entry */
+                {
                     fkpr_head = fk->next;
+                }
                 free (fk);
             }
             break;
@@ -356,6 +334,9 @@ void ARCH_DEP(hdiagf18_FC) (U32 options, VADR cmpb, REGS *regs)
     U32    ghandle = 0;         /* Guest file descriptor */
     struct fkeeper *fk = NULL;  /* Host file structure */
     struct fkeeper *rfk = NULL; /* Restart structure */
+#ifdef DYN76_DEBUG
+    int    io_error = 0;        /* Saves the errno for log messages */
+#endif
     
     /* Pseudo register contents are cached here once retrieved */
     U32    R0;
@@ -412,16 +393,25 @@ void ARCH_DEP(hdiagf18_FC) (U32 options, VADR cmpb, REGS *regs)
         
         /* If read, write, seek, commit, close or setmode */
         /* convert the file descriptor into a file handle */
-        fk = FindFK(ghandle);
+#ifdef DYN76_DEBUG
+        LOGMSG("DF18: CFILE - looking for guest file descriptor %d\n", ghandle);
+#endif
+        fk = FindFK(ghandle, &fkpr_head);
+#ifdef DYN76_DEBUG
+        LOGMSG("DF18: CFILE - guest file descriptor %d found at %X\n", 
+               ghandle, fk);
+#endif
         if (!fk)
-        {   /* Did not find the file descriptor - generate an exception */
-            
+        {   /* Did not find the guest file descriptor.
+               Treat it as if it were a bad host file handle */
 #ifdef DYN76_DEBUG
             LOGMSG("DF18: CFILE - guest file descriptor not found: %d\n", ghandle);
 #endif
+            R15 = -1 * EBADF;
+            set_reg(15,R15);
             R0 = 0;
             set_reg(0,R0); /* No restart on a failure */
-            ARCH_DEP(program_interrupt) (regs, PGM_OPERAND_EXCEPTION);
+            return;
         }
         handle = fk->handle;  /* All host file accesses use this variable */
 #ifdef DYN76_DEBUG
@@ -483,12 +473,12 @@ void ARCH_DEP(hdiagf18_FC) (U32 options, VADR cmpb, REGS *regs)
     if (R1 == 8) 
     {
 #ifdef DYN76_DEBUG
-        LOGMSG("DF18: CFILE - close file operation\n");
+        LOGMSG("DF18: CFILE - close file operation on host handle %d\n", handle);
 #endif
         R15 = _close (handle);
         set_reg(15,R15);
         if (R15 == 0)
-        {   RemoveFKByID (fk->id);
+        {   RemoveFKByID (fk->id, &fkpr_head, DO_FREE);
         }
         else
         {   R15 = -errno;
@@ -531,7 +521,9 @@ void ARCH_DEP(hdiagf18_FC) (U32 options, VADR cmpb, REGS *regs)
         get_reg(R4,4);
         R15 = _lseek (handle, R3, R4);
         if (R15 == (U32)-1)
+        {
             R15 = -errno;
+        }
         set_reg(15,R15);
         return;
     }
@@ -569,10 +561,13 @@ void ARCH_DEP(hdiagf18_FC) (U32 options, VADR cmpb, REGS *regs)
            must be removed in stage 3
         */
         dolock (nfile_lock);    /* Take ownership of the list */
-        R5 = nfile_id++;        /* safely increment the id counter */
-        rfk->id = R5;
-        rfk->next = fkpr_head;  /* Add to the top of the list */
-        fkpr_head = rfk;        /* New potential entry becomes the first */
+
+        R5 = restart_id++;        /* safely increment the id counter */
+#ifdef DYN76_DEBUG
+        LOGMSG("DF18: CFILE - adding restart fkeeper to rst_head list\n");
+#endif
+        AddFKByID(R5, rfk, &rst_head);
+
         unlock (nfile_lock);    /* Release ownership of the list */
         set_reg(5,R5);          /* Set the restart state for this new operation */
         
@@ -591,7 +586,7 @@ void ARCH_DEP(hdiagf18_FC) (U32 options, VADR cmpb, REGS *regs)
     
     {   /* Must have restarted, refresh fk */
         get_reg(R5,5);    /* R5 contains the previous restart id */
-        rfk = FindFK(R5);
+        rfk = FindFK(R5, &rst_head);
         if (!rfk)
         {   /* Did not find the restart - generate an exception */
             R0 = 0;
@@ -790,13 +785,29 @@ void ARCH_DEP(hdiagf18_FC) (U32 options, VADR cmpb, REGS *regs)
             /* Save the handle for use in other operations */
             rfk->handle = R15;  /* Save the host handle */
             /* This restart ID becomes the guest file descriptor */
-            R15 = rfk->id;
-            set_reg(15,R15);
 #ifdef DYN76_DEBUG
             LOGMSG("DF18: CFILE - opened host file handle: %d\n", rfk->handle);
-            LOGMSG("DF18: CFILE - opened guest file descriptor: %d\n", rfk->id);
 #endif
-            rfk = NULL;                   
+
+            /* Transfer the restart fkeeper to the open file fkeeper list */
+#ifdef DYN76_DEBUG
+            LOGMSG("DF18: CFILE - removing  w/o freeing fkeeper from restart list rst_head\n");
+#endif
+            RemoveFKByID (rfk->id, &rst_head, NO_FREE);
+            dolock(nfile_lock);
+#ifdef DYN76_DEBUG
+            LOGMSG("DF18: CFILE - adding fkeeper to open file list with fkpr_head\n");
+#endif
+            AddFKByID(nfile_id++, rfk, &fkpr_head);
+#ifdef DYN76_DEBUG
+            LOGMSG("DF18: CFILE - opened guest file descriptor: %d\n", rfk->id);
+#endif            
+            set_reg(15,R15);
+            unlock(nfile_lock);
+
+            R15 = rfk->id;
+            set_reg(15,R15);
+            rfk = NULL;    
             /* Note: during the start of the interruptable open operation the
                fkeeper structure was linked to the fkeeper list to allow restart 
                of the operation from the restart id.  Now that we have been 
@@ -851,13 +862,13 @@ void ARCH_DEP(hdiagf18_FC) (U32 options, VADR cmpb, REGS *regs)
 #endif
         while (R4 != 0) 
         {
-            if (rfk->data == 0) { /* Need to fill our buffer with some data? */
+            if (rfk->data == 0) 
+            { /* Need to fill our buffer with some data? */
                 i = R4;
                 
                 if (i > 256) 
                     i = 256;
                 
-                /*get_reg(R3,3); */
                 rfk->data = _read (handle, rfk->filename, i);
 #ifdef DYN76_DEBUG
                 LOGMSG("DF18: CFILE - host read result: %d\n", rfk->data);
@@ -871,13 +882,21 @@ void ARCH_DEP(hdiagf18_FC) (U32 options, VADR cmpb, REGS *regs)
                 set_reg(15,R15);
             }
             if (rfk->data <= 0)
-                /* All done, we reached EOF (or an error occured) */
+            {   /* All done, we reached EOF (or an error occured) */
                 break;
+            }
 
             /* Move the data read to the guest's storage */
             i = rfk->data - 1;
+            /* wstorec was designed to operate with storage-to-storage instructions.
+               The instruction length field is always one less than the number of
+               bytes involved in the instruction.  Hence the number of bytes to be
+               moved to storage is decremented by 1 to conform with this behavior. */
             
-            /* WARNING: This is where interruption might occur */
+            /* WARNING: This is where an interruption might occur.  An exception
+               will "nullify" the storing operation.
+               On restart, the read above will be bypassed and the entire sequence
+               of bytes, upto 256, will be restored */
             ARCH_DEP(wstorec) 
                 (rfk->filename,  /* This member is used as a buffer */
                  (unsigned char)i, 
@@ -919,55 +938,77 @@ void ARCH_DEP(hdiagf18_FC) (U32 options, VADR cmpb, REGS *regs)
         while (R4 != 0) 
         {
             i = R4 - 1;
+            /* wfetchc was designed to operate with storage-to-storage instructions.
+               The instruction length field is always one less than the number of
+               bytes involved in the instruction.  Hence the number of bytes to be
+               retrieved from storage  is decremented by 1 to conform with this 
+               behavior. */
 
             /* Move guest's write data to the internal buffer */
-            if (i > 255) 
+            if (i > 255)
+            {
                 i = 255;
+            }
             
-            /* WARNING: This is where interruption might occur */
+            /* WARNING: This is where interruption might occur.  The exception
+               "nullifies" the fetch operation.
+               On restart, the entire sequence of up to 256 bytes will be refetched
+               from storage */
             ARCH_DEP(wfetchc) 
                 (rfk->filename, 
                  (unsigned char)i, 
                  R2, 
                  space_ctl, 
                  regs);   
-            i++;
-            
-            /* Exception, can recalculate if/when restart */
-            R2 += i;
-            set_reg(2,R2);
-            
-            /* Remember to stop when enough is written */
-            R4 -= i;
-            set_reg(4,R4);
+            i++;  /* Fix up number of bytes stored to actual count */
 
             if (fk->mode)
                 MemConverter (rfk->filename, DCCebcdic_to_ascii, i);
                 /* rfk->filename is being used as a data buffer here */
             
             /* Write to the host file from the internal buffer */
+#ifdef DYN76_DEBUG
+            LOGMSG("DF18: CFILE - executing _write(%d, rfk->filename, %d)\n",
+                    handle, i);
+#endif   
             i = _write (handle, rfk->filename, i);
-            
+#ifdef DYN76_DEBUG
+            LOGMSG("DF18: CFILE - write with host handle %d resulted in %d\n",
+                    handle, i);
+#endif   
             if (i < 0)
             {
+#ifdef DYN76_DEBUG
+            io_error = errno;
+            LOGMSG("DF18: CFILE - write errno: %d\n", io_error);
+#endif 
                 R15 = -errno;
                 set_reg(15,R15);
-            }
-            
-            if (i <= 0)
-                /* We ran out of space (or an error occured) */
                 break;
-            
-            /* Return accumulated total */
+            }
+
+            /* Not an error, so 'i' is the number of bytes actually written */
+            /* Update the address pointer and remaining bytes to write */
+            R2 += i;
+            set_reg(2,R2);
+            R4 -= i;
+            set_reg(4,R4);
+            /* update the accumlated total */
             R15 += i;
             set_reg(15,R15);
         }
-        R5 = fk->SaveArea;
+        R5 = rfk->SaveArea;
         set_reg(5,R5);
     }
     
     if (rfk) /* clean up, unless open already has */
-        RemoveFKByID (rfk->id);  /* Safely remove from the restart state */
+    {
+#ifdef DYN76_DEBUG
+        LOGMSG("DF18: CFILE - removing and freeing restart fkeeper in rst_head list\n");
+#endif
+        /* Safely remove from the restart state */
+        RemoveFKByID (rfk->id, &rst_head, DO_FREE);
+    }
     
     /* Make sure we do not restart this operation */
     R0 = 0;
