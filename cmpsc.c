e2f1dce9e96bac861b0ac606643063db0bd8e3d8
Bernard van der Helm <bernard.van.der.helm@ygmail.com>
2012-04-18 18:59:00 +0200

    Delete and document #pragma Documentation Optimalization

--- cmpsc.c
+++ cmpsc.c
@@ -205,9 +205,6 @@
 #define GR0_e(regs)          ((regs)->GR_L(0) & 0x00000100)
 #define GR0_f1(regs)         ((regs)->GR_L(0) & 0x00000200)
 #define GR0_st(regs)         ((regs)->GR_L(0) & 0x00010000)
-/* We recognize the zp flag, but we were running a similar zero padding for   */
-/* years by writing or reading 8 index symbols at a time. 8 index symbols     */
-/* always fit within a byte boundary!                                         */
 #define GR0_zp(regs)         ((regs)->GR_L(0) & 0x00020000)
 
 /*----------------------------------------------------------------------------*/
@@ -483,8 +480,8 @@ static void ARCH_DEP(compress)(int r1, int r2, REGS *regs, REGS *iregs)
   srclen = GR_A(r2 + 1, iregs);
 
   /* Initialize compression context */
-  memset(cc.dea, 0, sizeof(cc.dea));
   cc.dctsz = GR0_dctsz(regs);
+  memset(cc.dea, 0, sizeof(cc.dea));
   cc.dest = NULL;
   for(i = 0; i < (0x01 << GR0_cdss(regs)); i++)
   {
@@ -503,55 +500,49 @@ static void ARCH_DEP(compress)(int r1, int r2, REGS *regs, REGS *iregs)
   cc.st = GR0_st(regs);
 
   /* Process individual index symbols until cbn bocomes zero */
-  if(unlikely(GR1_cbn(iregs)))
+  while(unlikely(GR1_cbn(iregs)))
   {
- 
-#ifdef OPTION_CMPSC_DEBUG
-    WRMSG(HHC90313, "D");
-#endif /* #ifdef OPTION_CMPSC_DEBUG */
+    /* Get the next character, return on end of source */
+    if(unlikely(ARCH_DEP(fetch_ch)(&cc, &ch)))
+      return;
 
-    while(likely(GR1_cbn(iregs)))
-    {
-      /* Get the next character, return on end of source */
-      if(unlikely(ARCH_DEP(fetch_ch)(&cc, &ch)))
-        return;
+    /* Set last match (alphabet entry) and initiate search */
+    ADJUSTREGS(cc.r2, cc.regs, cc.iregs, 1);
+    cc.dead_end = 1;
+    is = ch;
 
-      /* Set last match (alphabet entry) and initiate search */
-      ADJUSTREGS(cc.r2, cc.regs, cc.iregs, 1);
-      cc.dead_end = 1;
-      is = ch;
-
-      /* Do normal searching on eos or unknown dead end */
-      if(ARCH_DEP(fetch_ch)(&cc, &ch) || !cc.dea[is][ch])
-      {      
-        /* Get the alphabet entry and try to find a child */
-        cc.cce = ARCH_DEP(fetch_cce)(&cc, is);
-        while(ARCH_DEP(search_cce)(&cc, &ch, &is));
-        if(cc.dead_end)
-        {
-          /* We found a dead end combination */
-          cc.dea[is][ch] = 1;
-
-#ifdef OPTION_CMPSC_DEBUG
-          WRMSG(HHC90365, "D", is, ch, "found");
-#endif /* #ifdef OPTION_CMPSC_DEBUG */
-
-        }
-      }
-      else
+    /* Do normal searching on eos or unknown dead end */
+    if(ARCH_DEP(fetch_ch)(&cc, &ch) || !cc.dea[is][ch])
+    {      
+      /* Get the alphabet entry and try to find a child */
+      cc.cce = ARCH_DEP(fetch_cce)(&cc, is);
+      while(ARCH_DEP(search_cce)(&cc, &ch, &is));
+      if(cc.dead_end)
       {
+        /* We found a dead end combination */
+        cc.dea[is][ch] = 1;
 
 #ifdef OPTION_CMPSC_DEBUG
-        WRMSG(HHC90365, "D", is, ch, "encountered");
+        WRMSG(HHC90365, "D", is, ch, "found");
 #endif /* #ifdef OPTION_CMPSC_DEBUG */
 
-      }	
-
-      /* Write the last match and commit */
-      if(unlikely(ARCH_DEP(store_is)(&cc, is)))
-        return;
-      COMMITREGS(regs, iregs, r1, r2);
+      }
     }
+    else
+    {
+
+#ifdef OPTION_CMPSC_DEBUG
+      WRMSG(HHC90365, "D", is, ch, "encountered");
+#endif /* #ifdef OPTION_CMPSC_DEBUG */
+
+    }	
+
+    /* Write the last match, return on end of destination */
+    if(unlikely(ARCH_DEP(store_is)(&cc, is)))
+      return;
+
+    /* Commit registers */
+    COMMITREGS(regs, iregs, r1, r2);
   }
 
   /* Block processing, cbn stays zero */
@@ -601,8 +592,6 @@ static void ARCH_DEP(compress)(int r1, int r2, REGS *regs, REGS *iregs)
 #endif /* #ifdef OPTION_CMPSC_DEBUG */
 
       }
-
-      /* Write the last match, this can be the alphabet entry */
       cc.is[i] = is;
 
 #ifdef OPTION_CMPSC_DEBUG
@@ -611,7 +600,7 @@ static void ARCH_DEP(compress)(int r1, int r2, REGS *regs, REGS *iregs)
 
     }
 
-    /* Write last matches and commit */
+    /* Write index symbols and commit */
     ARCH_DEP(store_iss)(&cc);
     COMMITREGS2(regs, iregs, r1, r2);
 
@@ -665,9 +654,11 @@ static void ARCH_DEP(compress)(int r1, int r2, REGS *regs, REGS *iregs)
 
     }
 
-    /* Write the last match and commit */
+    /* Write the last match, return on end of destination */
     if(unlikely(ARCH_DEP(store_is)(&cc, is)))
       return;
+
+    /* Commit registers */
     COMMITREGS(regs, iregs, r1, r2);
   }
 }
@@ -691,6 +682,7 @@ static BYTE *ARCH_DEP(fetch_cce)(struct cc *cc, unsigned index)
   print_cce(cce);
 #endif /* #ifdef OPTION_CMPSC_DEBUG */
 
+  /* Check for data exception */
   cct = CCE_cct(cce);
   if(cct < 2)
   {
@@ -948,6 +940,8 @@ static int ARCH_DEP(search_cce)(struct cc *cc, BYTE *ch, U16 *is)
       {
         /* Child is tested, so stop searching for siblings*/
         ind_search_siblings = 0;
+
+        /* This is not a dead end */
         cc->dead_end = 0;
 
         /* Check if child should not be examined */
@@ -956,15 +950,11 @@ static int ARCH_DEP(search_cce)(struct cc *cc, BYTE *ch, U16 *is)
           /* No need to examine child, found the last match */
           ADJUSTREGS(cc->r2, cc->regs, cc->iregs, 1);
           *is = CCE_cptr(cc->cce) + i;
-
-          /* Index symbol is found, stop searching */
           return(0);
         }
 
-        /* Found a child get the character entry */
+        /* Found a child get the character entry and check if additional extension characters match */
         ccce = ARCH_DEP(fetch_cce)(cc, CCE_cptr(cc->cce) + i);
-
-        /* Check if additional extension characters match */
         if(likely(ARCH_DEP(test_ec)(cc, ccce)))
         {
           /* Set last match */
@@ -975,10 +965,8 @@ static int ARCH_DEP(search_cce)(struct cc *cc, BYTE *ch, U16 *is)
           WRMSG(HHC90338, "D", *is);
 #endif  /* #ifdef OPTION_CMPSC_DEBUG */
 
-          /* Found a matching child, make it parent */
+          /* Found a matching child, make it parent and keep searching */
           cc->cce = ccce;
-
-          /* We found a parent, keep searching */
           return(1);
         }
       }
@@ -1010,19 +998,11 @@ static int ARCH_DEP(search_sd)(struct cc *cc, BYTE *ch, U16 *is)
   int y_in_parent;                     /* Indicator if y bits are in parent   */
 
   /* Initialize values */
-
   ind_search_siblings = 1;
-
-  /* For the first sibling descriptor y bits are in the cce parent */
+  sd_ptr = CCE_ccs(cc->cce);
+  searched = sd_ptr;
   y_in_parent = 1;
 
-  /* Sibling follows last possible child */
-  sd_ptr = CCE_ccs(cc->cce);
-
-  /* set searched childs */
-  searched = sd_ptr;
-
-  /* As long there are sibling characters */
   do
   {
     /* Get the sibling descriptor */
@@ -1052,11 +1032,6 @@ static int ARCH_DEP(search_sd)(struct cc *cc, BYTE *ch, U16 *is)
     }
     else
     {
-      /* Prevent gcc warning for sd2 */
-#if __GNUC__ >= 4 && __GNUC_MINOR__ >= 6
-  #pragma GCC diagnostic push
-  #pragma GCC diagnostic ignored "-Wuninitialized"
-#endif /* #ifdef __GNUC__ */
 
 #ifdef OPTION_CMPSC_DEBUG
       WRMSG(HHC90340, "D", CCE_cptr(cc->cce) + sd_ptr);
@@ -1069,18 +1044,20 @@ static int ARCH_DEP(search_sd)(struct cc *cc, BYTE *ch, U16 *is)
     scs = SD_scs(cc->f1, sd1);
     for(i = 0; i < scs; i++)
     {
+
+//Please ignore following gcc warnings, sd2 is only initialized and used when f1 flag = 1
+//cmpsc.c:1053:10: warning: 'sd2' may be used uninitialized in this function [-Wuninitialized]
+//cmpsc.c:995:9: note: 'sd2' was declared here
+
       /* Stop searching when child tested and no consecutive child character */
       if(unlikely(!ind_search_siblings && !SD_ccc(cc->f1, sd1, sd2, i)))
         return(0);
-
-#if __GNUC__ >= 4 && __GNUC_MINOR__ >= 6
-  #pragma GCC diagnostic pop
-#endif /* #ifdef __GNUC__ */
-
       if(unlikely(*ch == SD_sc(cc->f1, sd1, sd2, i)))
       {
         /* Child is tested, so stop searching for siblings*/
         ind_search_siblings = 0;
+
+	/* this is not a dead end */
         cc->dead_end = 0;
 
         /* Check if child should not be examined */
@@ -1089,15 +1066,11 @@ static int ARCH_DEP(search_sd)(struct cc *cc, BYTE *ch, U16 *is)
           /* No need to examine child, found the last match */
           ADJUSTREGS(cc->r2, cc->regs, cc->iregs, 1);
           *is = CCE_cptr(cc->cce) + sd_ptr + i + 1;
-
-          /* Index symbol found */
           return(0);
         }
 
-        /* Found a child get the character entry */
+        /* Found a child get the character entry and check if additional extension characters match */
         ccce = ARCH_DEP(fetch_cce)(cc, CCE_cptr(cc->cce) + sd_ptr + i + 1);
-
-        /* Check if additional extension characters match */
         if(unlikely(ARCH_DEP(test_ec)(cc, ccce)))
         {
           /* Set last match */
@@ -1108,10 +1081,8 @@ static int ARCH_DEP(search_sd)(struct cc *cc, BYTE *ch, U16 *is)
           WRMSG(HHC90341, "D", *is);
 #endif /* #ifdef OPTION_CMPSC_DEBUG */
 
-          /* Found a matching child, make it parent */
+          /* Found a matching child, make it parent and keep searching */
           cc->cce = ccce;
-
-          /* We found a parent */
           return(1);
         }
       }
@@ -1396,7 +1367,9 @@ static int ARCH_DEP(test_ec)(struct cc *cc, BYTE *cce)
 
   if(CCE_ecs(cce))
   {
+    /* This is not a dead end */
     cc->dead_end = 0;
+
     for(i = 0; i < CCE_ecs(cce); i++)
     {
       if(unlikely(GR_A(cc->r2 + 1, cc->iregs) <= (U32) i + 1))
@@ -1478,24 +1451,21 @@ static void ARCH_DEP(expand)(int r1, int r2, REGS *regs, REGS *iregs)
   ec.src = NULL;
 
   /* Process individual index symbols until cbn becomes zero */
-  if(unlikely(GR1_cbn(iregs)))
+  while(unlikely(GR1_cbn(iregs)))
   {
-    while(likely(GR1_cbn(iregs)))
+    if(unlikely(ARCH_DEP(fetch_is)(&ec, &is)))
+      return;
+    if(likely(!ec.ecl[is]))
     {
-      if(unlikely(ARCH_DEP(fetch_is)(&ec, &is)))
+      ec.ocl = 0;                      /* Initialize output cache             */
+      ARCH_DEP(expand_is)(&ec, is);
+      if(unlikely(ARCH_DEP(vstore)(&ec, ec.oc, ec.ocl)))
         return;
-      if(likely(!ec.ecl[is]))
-      {
-        ec.ocl = 0;                    /* Initialize output cache             */
-        ARCH_DEP(expand_is)(&ec, is);
-        if(unlikely(ARCH_DEP(vstore)(&ec, ec.oc, ec.ocl)))
-          return;
-      }
-      else
-      {
-        if(unlikely(ARCH_DEP(vstore)(&ec, &ec.ec[ec.eci[is]], ec.ecl[is])))
-          return;
-      }
+    }
+    else
+    {
+      if(unlikely(ARCH_DEP(vstore)(&ec, &ec.ec[ec.eci[is]], ec.ecl[is])))
+        return;
     }
 
     /* Commit, including GR1 */
@@ -1509,11 +1479,24 @@ static void ARCH_DEP(expand)(int r1, int r2, REGS *regs, REGS *iregs)
     ec.ocl = 0;                        /* Initialize output cache             */
     for(i = 0; i < 8; i++)
     {
-      /* Prevent warning for iss */
-#if __GNUC__ >= 4 && __GNUC_MINOR__ >= 6
-  #pragma GCC diagnostic push
-  #pragma GCC diagnostic ignored "-Wuninitialized"
-#endif /* #ifdef __GNUC__ */
+
+//Please ignore following gcc warnings, iss array is filled by fetch_iss!
+//cmpsc.c:1505:10: warning: 'iss[7]' may be used uninitialized in this function [-Wuninitialized]
+//cmpsc.c:1423:7: note: 'iss[7]' was declared here
+//cmpsc.c:1505:10: warning: 'iss[6]' may be used uninitialized in this function [-Wuninitialized]
+//cmpsc.c:1423:7: note: 'iss[6]' was declared here
+//cmpsc.c:1505:10: warning: 'iss[5]' may be used uninitialized in this function [-Wuninitialized]
+//cmpsc.c:1423:7: note: 'iss[5]' was declared here
+//cmpsc.c:1505:10: warning: 'iss[4]' may be used uninitialized in this function [-Wuninitialized]
+//cmpsc.c:1423:7: note: 'iss[4]' was declared here
+//cmpsc.c:1505:10: warning: 'iss[3]' may be used uninitialized in this function [-Wuninitialized]
+//cmpsc.c:1423:7: note: 'iss[3]' was declared here
+//cmpsc.c:1505:10: warning: 'iss[2]' may be used uninitialized in this function [-Wuninitialized]
+//cmpsc.c:1423:7: note: 'iss[2]' was declared here
+//cmpsc.c:1505:10: warning: 'iss[1]' may be used uninitialized in this function [-Wuninitialized]
+//cmpsc.c:1423:7: note: 'iss[1]' was declared here
+//cmpsc.c:1505:10: warning: 'iss[0]' may be used uninitialized in this function [-Wuninitialized]
+//cmpsc.c:1423:7: note: 'iss[0]' was declared here
 
 #ifdef OPTION_CMPSC_DEBUG
       WRMSG(HHC90347, "D", iss[i], i);
@@ -1526,11 +1509,6 @@ static void ARCH_DEP(expand)(int r1, int r2, REGS *regs, REGS *iregs)
         memcpy(&ec.oc[ec.ocl], &ec.ec[ec.eci[iss[i]]], ec.ecl[iss[i]]);
         ec.ocl += ec.ecl[iss[i]];
       }
-
-#if __GNUC__ >= 4 && __GNUC_MINOR__ >= 6
-  #pragma GCC diagnostic pop
-#endif /* #ifdef __GNUC__ */
-
     }
 
     /* Write and commit, cbn unchanged, so no commit for GR1 needed */
