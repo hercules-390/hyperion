46a922c0dceb17486e604d095a997d3bc726c1f3
Jan Jaeger <jan.jaeger@westnet.com.au>
2012-07-06 13:31:30 +1000

    Always use devhnd for track read calls

--- ckddasd.c
+++ ckddasd.c
@@ -771,7 +771,7 @@ int     i;                              /* Index                     */
 BYTE    unitstat;                       /* Unit Status               */
 
     /* Write the last track image if it's modified */
-    ckddasd_read_track (dev, -1, &unitstat);
+    (dev->hnd->read) (dev, -1, &unitstat);
 
     /* Free the cache */
     cache_lock(CACHE_DEVBUF);
@@ -825,7 +825,7 @@ int             trk;                    /* Track number              */
 /*-------------------------------------------------------------------*/
 /* Return track image length                                         */
 /*-------------------------------------------------------------------*/
-static int ckd_trklen (DEVBLK *dev, BYTE *buf)
+int ckd_trklen (DEVBLK *dev, BYTE *buf)
 {
 int             sz;                     /* Size so far               */
 
@@ -1183,13 +1183,12 @@ void ckddasd_end (DEVBLK *dev)
 BYTE    unitstat;                       /* Unit Status               */
 
     /* Write the last track image if it's modified */
-    ckddasd_read_track (dev, -1, &unitstat);
+    (dev->hnd->read) (dev, -1, &unitstat);
 }
 
 /*-------------------------------------------------------------------*/
 /* Return used cylinders                                             */
 /*-------------------------------------------------------------------*/
-static
 int ckddasd_used (DEVBLK *dev)
 {
     return dev->ckdcyls;
