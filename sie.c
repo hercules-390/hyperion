2ceca2f33e83c74d388f0021ed3df696b9b31d79
Fish (David B. Trout) <fish@infidels.org>
2013-03-19 09:25:09 -0700

    Don't memset until we know malloc succeeded! (DOH!)

--- sie.c
+++ sie.c
@@ -280,17 +280,17 @@ U64     dreg;
 
      /* Initialize guestregs if first time */
      if (GUESTREGS == NULL)
-     {
+    {
         GUESTREGS = malloc_aligned(sizeof(REGS), 4096);
-        memset( GUESTREGS, 0, sizeof(REGS) );
         if (GUESTREGS == NULL)
-         {
-             logmsg(MSG(HHC00813, "E", PTYPSTR(regs->cpuad), regs->cpuad, "calloc()", strerror(errno)));
+        {
+            logmsg(MSG(HHC00813, "E", PTYPSTR(regs->cpuad), regs->cpuad, "calloc()", strerror(errno)));
 #if !defined(NO_SIGABEND_HANDLER)
-             signal_thread(sysblk.cputid[regs->cpuad], SIGUSR1);
+            signal_thread(sysblk.cputid[regs->cpuad], SIGUSR1);
 #endif
-             return;
-         }
+            return;
+        }
+        memset( GUESTREGS, 0, sizeof(REGS) );
         cpu_init (regs->cpuad, GUESTREGS, regs);
      }
 
