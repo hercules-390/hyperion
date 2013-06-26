537445ae87f11cebda310cc03a3eb2cba054e2cd
Fish (David B. Trout) <fish@infidels.org>
2013-06-26 09:14:45 -0700

    (sigh! fix a comment typo of my own this time)

--- hmalloc.h
+++ hmalloc.h
@@ -22,7 +22,7 @@
 /*        be used which is in line with most commodity processors     */
 /*        as of 2013. The cache line size must be a power of 2.       */
 /*        The minimum recognized cache line size is 32, and the       */
-/*        max imum recognized cache line size is 4096.                */
+/*        maximum recognized cache line size is 4096.                 */
 /*--------------------------------------------------------------------*/
 #if defined(CACHE_LINE_SIZE)
     #if !(CACHE_LINE_SIZE ==   32 || \
