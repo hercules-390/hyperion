40c354aed86a07864a1d9deafaab857a90566cc8
dasdman <mgaubatz@groupgw.com>
2013-07-16 15:31:36 -0700

    Processor I/O interrupt incorrectly displayed  while tracing in 370 EC mode.

--- msgenu.h
+++ msgenu.h
@@ -645,7 +645,7 @@ do { \
 #define HHC00801 "Processor %s%02X: %s%s %s code %4.4X  ilc %d%s"
 #define HHC00802 "Processor %s%02X: PER event: code %4.4X perc %2.2X addr "F_VADR
 #define HHC00803 "Processor %s%02X: program interrupt loop PSW %s"
-#define HHC00804 "Processor %s%02X: I/O interrupt code %4.4X CSW %2.2X%2.2X%2.2X%2.2X %2.2X%2.2X%2.2X%2.2X"
+#define HHC00804 "Processor %s%02X: I/O interrupt code %1.1X:%4.4X CSW %2.2X%2.2X%2.2X%2.2X %2.2X%2.2X%2.2X%2.2X"
 #define HHC00805 "Processor %s%02X: I/O interrupt code %8.8X parm %8.8X"
 #define HHC00806 "Processor %s%02X: I/O interrupt code %8.8X parm %8.8X id %8.8X"
 #define HHC00807 "Processor %s%02X: machine check code %16.16"I64_FMT"u"
