099d1383a8c5991e83922b973c848c5f50a8bde2
dasdman <mgaubatz@groupgw.com>
2013-08-10 12:38:04 -0700

    Channel routine iobuf_validate incorrectly freeing I/O buffer resulting in a free memory failure in iobuf_destroy/free_aligned.

--- channel.c
+++ channel.c
@@ -155,13 +155,12 @@ iobuf_create (u_int size)
 static INLINE u_int
 iobuf_validate (IOBUF *iobuf)
 {
-    if (iobuf == NULL                                           ||
-        iobuf->size < 65536                                     ||
-        iobuf->start != (BYTE *)iobuf->data                     ||
-        iobuf->end != (BYTE *)iobuf->start + (iobuf->size - 1 ))
-        return 0;
-    free_aligned(iobuf);
-    return 1;
+    return
+    ((iobuf == NULL                         ||
+      iobuf->size < 65536                   ||
+      iobuf->start != (BYTE *)iobuf->data   ||
+      iobuf->end != (BYTE *)iobuf->start + (iobuf->size - 1 )) ?
+     0 : 1);
 }
 
 static INLINE void
