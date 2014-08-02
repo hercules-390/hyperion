hercules-390/hyperion

commit 02ca3a5bf50ec8c6d6a75b87bcce4773317512d5

Bypass MS VC Bug ID 363375 (compiler failure).

hmacros.h
@@ -94,7 +94,7 @@
 /*-------------------------------------------------------------------*/
 /*      CASSERT macro       a compile time assertion check           */
 /*-------------------------------------------------------------------*/
-/** 
+/**
  *  Validates at compile time whether condition is true without
  *  generating code. It can be used at any point in a source file
  *  where typedefs are legal.
@@ -950,5 +950,34 @@ do { \
 #define RECV_SOCKDEV_THREAD_PIPE_SIGNAL()  RECV_PIPE_SIGNAL( sysblk.sockrpipe, sysblk.sockpipe_lock, sysblk.sockpipe_flag )
 #define SIGNAL_CONSOLE_THREAD()            SEND_PIPE_SIGNAL( sysblk.cnslwpipe, sysblk.cnslpipe_lock, sysblk.cnslpipe_flag )
 #define SIGNAL_SOCKDEV_THREAD()            SEND_PIPE_SIGNAL( sysblk.sockwpipe, sysblk.sockpipe_lock, sysblk.sockpipe_flag )
+
+
+/*********************************************************************/
+/*                                                                   */
+/*  Define compiler error bypasses                                   */
+/*                                                                   */
+/*********************************************************************/
+
+
+/*      MS VC Bug ID 363375 Bypass
+ *
+ *      Note: This error, or similar, also occurs at VS 2010 and
+ *            VS 2012.
+ *
+ *      TODO: Verify if fixed in VS 2013 and/or VS 2014.
+ *
+ */
+
+#if ( _MSC_VER >= VS2008 )
+# define ENABLE_VS_BUG_ID_363375_BYPASS                                 \
+  __pragma( optimize( "", off ) )                                       \
+  __pragma( optimize( "t", on ) )
+# define DISABLE_VS_BUG_ID_363375_BYPASS                                \
+  __pragma( optimize( "", on ) )
+#else
+# define ENABLE_VS_BUG_ID_363375_BYPASS
+# define DISABLE_VS_BUG_ID_363375_BYPASS
+#endif
+
 
 #endif // _HMACROS_H

ctc_ptp.c
@@ -1613,6 +1613,7 @@ void  ptp_read( DEVBLK* pDEVBLK, U32  uCount,
 /* ------------------------------------------------------------------ */
 // Note: The caller must hold the PTPBLK->ReadBufferLock.
 
+ENABLE_VS_BUG_ID_363375_BYPASS
 void  read_read_buffer( DEVBLK* pDEVBLK,   U32     uCount,
                         int     iCCWSeq,   BYTE*   pIOBuf,
                         BYTE*   pMore,     BYTE*   pUnitStat,
@@ -1799,6 +1800,7 @@ void  read_read_buffer( DEVBLK* pDEVBLK,   U32     uCount,
 
     return;
 }   /* End function  read_read_buffer() */
+DISABLE_VS_BUG_ID_363375_BYPASS
 
 
 /* ------------------------------------------------------------------ */

commit 49261f4b5ceff28ae76bcf6e4d6d38df3cc568fb

Commit 02ca3a5 broke Linux compile
Thanks to Enrico Sorichetti for the correction.

hmacros.h
@@ -968,7 +968,7 @@ do { \
  *
  */
 
-#if ( _MSC_VER >= VS2008 )
+#if defined( _MSC_VER ) && ( _MSC_VER >= VS2008 )
 # define ENABLE_VS_BUG_ID_363375_BYPASS                                 \
   __pragma( optimize( "", off ) )                                       \
   __pragma( optimize( "t", on ) )