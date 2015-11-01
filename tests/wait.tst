hercules-390/hyperion

commit 87b551b77f5c12c19123c5cd5afcf8ad63fbea46

Fiddle with test cases and the testing engine.
Tests are now given .1 second to run (I'm not sure this makes a difference).

Add *Explain and *Message to engine.

Change runtest arguments to be closer to runtest.cmd.

Change "make check" to go to the right place rather than to a hardwired path.

tests/runtest
@@ -13,22 +13,38 @@
 # Having done "make" to build the executable, issue "./hercules" to make
 # libtool create an actual executable.
 
-# You  can  run a test case by "../hyperion/tests/runtest <fname>".  Be
-# sure  to  omit  the extension.  Omit the file name or specify '*' (an
-# asterisk  enclosed  in  single  quotes)  to  run  all  test that have
-# extension .tst.
+# Arguments:
+# 1     Path  from the current directory to the tests source directory.
+#       Specify - for the default ../hyperion/tests
+# 2     File  name  if  test  case  omitting  the .tst extension or the
+#       default * for all .tst files.  But see #4.
+# 3     Specify  noexit  to  suppress  the  exit  command that normally
+#       terminates  the test cases.  This will allow you to poke around
+#       fwith  console commands.  Anything else, including nothing adds
+#       an exit command at the end of the composite script.
+# 4     File type of test cases.  Default .tst.
 
-# Specify  noexit  as  the second argument to suppress the exit command
-# that normally terminates the test cases.  This will allow you to poke
-# around.
+# You  can  run  a  specific  test case by "../hyperion/tests/runtest -
+# <fname>".  Be sure to omit the extension.
+
+# Tests  are  normally  done alphabetically.  You can use the scripting
+# facilities  to  include specific test case files in any order, but do
+# not  give these embedded files the default extension (unless you want
+# them run twice!).
 
 wfn=allTests
-dir=../hyperion/tests
-fns=${1:-*}
-cat $dir/$fns.tst >$wfn.tst
-if [ "$2" != "noexit" ] ; then
+dir=${1:-../hyperion/tests}
+if [ "$dir" == "-" ] ; then
+        dir=../hyperion/tests
+fi
+
+fns=${2:-*}
+ftype=${4:-tst}
+
+cat $dir/$fns.$ftype >$wfn.tst
+if [ "$3" != "noexit" ] ; then
         echo exit >>$wfn.tst
 fi
+
 ./hercules -f $dir/tests.conf -r $wfn.tst >$wfn.out 2>&1
-sed -n -e '/^HHC01603I \*Testcase/,/^HHC01603I \*Done/p' >$wfn.sout $wfn.out
-rexx $dir/redtest.rexx $wfn.sout
+rexx $dir/redtest.rexx $wfn.out

commit 59697f18da8d7d7c84a5f848b45b4e77069f08c9

Test harness script 'wait' command:
A new script-only command called "wait" is introduced that waits for all processors to load a disabled wait PSW before continuing.

Its syntax is identical to the "pause" statement.  You specify on your "wait" statement the maximum number of seconds you are willing to wait for all CPUs to enter disabled wait.  If the time limit is exceeded the script issues a warning and continues anyway.  Otherwise it will continue immediately as soon as all CPUs have entered a disabled wait state.  A new 'wait.tst' test verifies proper functionality.

Both the "pause" statement as well as the new "wait" statement are now handled virtually identically by the new logic which also resolves (eliminates) the previous poorly designed 'polling" technique.  Canceling a script via the "cscript" command now occurs immediately.

The bug in the the *nix 'runtest' script introduced by commit 87b551b77f5c12c19123c5cd5afcf8ad63fbea46 is also fixed.

Hercules.vcproj
@@ -2280,6 +2280,10 @@
 					RelativePath=".\util\TMOUNT.txt"
 					>
 				</File>
+				<File
+					RelativePath=".\util\zzsacard.bin"
+					>
+				</File>
 			</Filter>
 			<Filter
 				Name="tests"
@@ -3075,6 +3079,10 @@
 							</File>
 							<File
 								RelativePath=".\tests\sske390.tst"
+								>
+							</File>
+							<File
+								RelativePath=".\tests\wait.tst"
 								>
 							</File>
 						</Filter>

cpu.c
@@ -1620,9 +1620,20 @@ cpustate_stopping:
         if( IS_IC_DISABLED_WAIT_PSW(regs) )
         {
             char buf[40];
+            // "Processor %s%02X: disabled wait state %s"
             WRMSG (HHC00809, "I", PTYPSTR(regs->cpuad), regs->cpuad, str_psw(regs, buf));
             regs->cpustate = CPUSTATE_STOPPING;
             RELEASE_INTLOCK(regs);
+
+            /* Let waiting script know about disabled wait. */
+            obtain_lock( &sysblk.scrlock );
+            if (sysblk.scrtest)
+            {
+                sysblk.scrtest++;
+                broadcast_condition( &sysblk.scrcond );
+            }
+            release_lock( &sysblk.scrlock );
+
             longjmp(regs->progjmp, SIE_NO_INTERCEPT);
         }
 

hstructs.h
@@ -594,6 +594,8 @@ struct SYSBLK {
         int     mbm;                    /* Measurement block mode    */
         int     mbd;                    /* Device connect time mode  */
         LOCK    scrlock;                /* Script processing lock    */
+        COND    scrcond;                /* Test script condition     */
+        int     scrtest;                /* 1 == test script running  */
         TID     cmdtid;                 /* Active command thread     */
         char   *cmdsep;                 /* Single Char cmd Sep       */
         BYTE    sysgroup;               /* Panel Command grouping    */

impl.c
@@ -619,6 +619,7 @@ int     dll_count;                      /* index into array          */
     initialize_lock (&sysblk.sigplock);
     initialize_lock (&sysblk.mntlock);
     initialize_lock (&sysblk.scrlock);
+    initialize_condition (&sysblk.scrcond);
     initialize_lock (&sysblk.crwlock);
     initialize_lock (&sysblk.ioqlock);
     initialize_condition (&sysblk.ioqcond);

msgenu.h
@@ -1245,7 +1245,7 @@ Examples:
 #define HHC02197 "Symbol name %s is reserved"
 /* 2198 moved to config.c                                            */
 #define HHC02199 "Symbol %-12s %s"
-// reserve 02200 - 02369 for command processing */
+// reserve 02200 - 02369 for command processing; script.c
 #define HHC02200 "%1d:%04X device not found"
 #define HHC02201 "Device number missing"
 #define HHC02202 "Missing argument(s). Type 'help %s' for assistance."
@@ -1376,6 +1376,9 @@ Examples:
 #define HHC02327 "%c:"F_RADR"  Storage address is not valid"
 #define HHC02328 "%c:"F_RADR"  Addressing exception"
 #define HHC02329 "%c:"F_VADR"  Translation exception %4.4hX  %s"
+#define HHC02330 "Config file[%d] %s: wait invalid for config files; pausing instead"
+#define HHC02331 "Script %d: Waiting %d milliseconds for CPU(s) to enter disabled wait..."
+#define HHC02332 "Script %d: Wait timeout"
 
 #define HHC02370 "%1d:%04X CU or LCU %s conflicts with existing CUNUM %04X SSID %04X CU/LCU %s"
 #define HHC02371 "%1d:%04X Adding device exceeds CU and/or LCU device limits"

script.c
@@ -44,6 +44,10 @@
  #undef   _GEN_ARCH
 #endif
 
+/* Forward declarations:                                             */
+struct SCRCTL;
+static int do_special(char *fname, int *inc_stmtnum, struct SCRCTL *pCtl, char *p);
+/* End of forward declarations.                                      */
 
 /*-------------------------------------------------------------------*/
 /* Subroutine to parse an argument string. The string that is passed */
@@ -201,52 +205,9 @@ char   *buf1;                           /* Pointer to resolved buffer*/
         if (stmtlen == 0 || buf[0] == '*' || buf[0] == '#')
            continue;
 
-        /* Special handling for 'pause' statement */
-        if (strncasecmp( buf, "pause ", 6 ) == 0)
-        {
-            double pauseamt     =  0.0;   /* (secs to pause) */
-            struct timespec ts  = {0,0};  /* (nanosleep arg) */
-            U64 i, nsecs        =   0;    /* (nanoseconds)   */
-
-            pauseamt = atof( &buf[6] );
-
-            if (pauseamt < 0.0 || pauseamt > 999.0)
-            {
-                // "Config file[%d] %s: error processing statement: %s"
-                WRMSG( HHC01441, "W", *inc_stmtnum, fname, "syntax error; statement ignored" );
-                continue; /* (go on to next statement) */
-            }
-
-            /* We sleep in 1/4 second increments at first */
-            nsecs = (U64) (pauseamt * 1000000000.0);
-            ts.tv_nsec = 250000000; /* 1/4th of a second */
-            ts.tv_sec  = 0;
-
-            if (MLVL( VERBOSE ))
-            {
-                // "Config file[%d] %s: processing paused for %d milliseconds..."
-                WRMSG( HHC02318, "I", *inc_stmtnum, fname, (int)(pauseamt * 1000.0) );
-            }
-
-            /* Sleep for 1/4 second increments */
-            for (i = nsecs; i >= (U64) ts.tv_nsec; i -= (U64) ts.tv_nsec)
-                nanosleep( &ts, NULL );
-
-            /* Sleep for remainder of time period */
-            if (i)
-            {
-                ts.tv_nsec = (long) i;
-                nanosleep( &ts, NULL );
-            }
-
-            if (MLVL( VERBOSE ))
-            {
-                // "Config file[%d] %s: processing resumed..."
-                WRMSG( HHC02319, "I", *inc_stmtnum, fname );
-            }
-
-            continue;  /* (go on to next statement) */
-        }
+        /* Special handling for 'pause' and 'wait' statements */
+        if (do_special(fname, inc_stmtnum, NULL, buf))
+            continue;
 
         break;
     } /* end while */
@@ -800,6 +761,7 @@ int cscript_cmd( int argc, char *argv[], char *cmdline )
         if (first)
         {
             pCtl->scr_flags |= SCR_CANCEL;
+            broadcast_condition( &sysblk.scrcond );
             release_lock( &sysblk.scrlock );
             return 0;
         }
@@ -815,6 +777,9 @@ int cscript_cmd( int argc, char *argv[], char *cmdline )
             break;
         }
     }
+
+    if (count)
+        broadcast_condition( &sysblk.scrcond );
 
     release_lock( &sysblk.scrlock );
 
@@ -957,57 +922,9 @@ int     rc;                             /* (work)                    */
         for (stmtlen = (int)strlen(p); stmtlen && isspace(p[stmtlen-1]); stmtlen--);
         p[stmtlen] = 0;
 
-        /* Special handling for 'pause' statement */
-        if (strncasecmp( p, "pause ", 6 ) == 0)
-        {
-            double pauseamt     =  0.0;   /* (secs to pause) */
-            struct timespec ts  = {0,0};  /* (nanosleep arg) */
-            U64 i, nsecs        =   0;    /* (nanoseconds)   */
-
-#if defined( ENABLE_BUILTIN_SYMBOLS )
-            {
-                char *secs = resolve_symbol_string( p+6 );
-                pauseamt = atof( secs );
-                free( secs );
-            }
-#else
-            pauseamt = atof( p+6 );
-#endif
-
-            if (pauseamt < 0.0 || pauseamt > 999.0)
-            {
-                // "Script %d: file statement only; %s ignored"
-                WRMSG( HHC02261, "W", pCtl->scr_id, p+6 );
-                continue; /* (go on to next statement) */
-            }
-
-            nsecs = (U64) (pauseamt * 1000000000.0);
-            ts.tv_nsec = 250000000; /* 1/4th of a second */
-            ts.tv_sec  = 0;
-
-            if (!script_abort( pCtl ) && MLVL( VERBOSE ))
-            {
-                // "Script %d: processing paused for %d milliseconds..."
-                WRMSG( HHC02262, "I", pCtl->scr_id, (int)(pauseamt * 1000.0) );
-            }
-
-            for (i = nsecs; i >= (U64) ts.tv_nsec && !script_abort( pCtl ); i -= (U64) ts.tv_nsec)
-                nanosleep( &ts, NULL );
-
-            if (i && !script_abort( pCtl ))
-            {
-                ts.tv_nsec = (long) i;
-                nanosleep( &ts, NULL );
-            }
-
-            if (!script_abort( pCtl ) && MLVL( VERBOSE ))
-            {
-                // "Script %d: processing resumed..."
-                WRMSG( HHC02263, "I", pCtl->scr_id );
-            }
-            continue;  /* (go on to next statement) */
-        }
-        /* (end 'pause' stmt) */
+        /* Special handling for 'pause' and 'wait' statements */
+        if (do_special(NULL, NULL, pCtl, p))
+            continue;
 
         /* Process statement as command */
         panel_command( p );
@@ -1163,5 +1080,157 @@ int script_cmd( int argc, char* argv[], char* cmdline )
     return 0;
 }
 /* end script_cmd */
+
+/*-------------------------------------------------------------------*/
+/* returns:  0 = not pause or wait stmt,  1 = pause/wait completed   */
+/*-------------------------------------------------------------------*/
+static int
+do_special(char *fname, int *inc_stmtnum, struct SCRCTL *pCtl, char *p)
+{
+    struct timeval beg, now, dur;   /* To calculate remaining time   */
+    double secs;                    /* Seconds to pause or wait      */
+    U32 msecs;                      /* Same thing in milliseconds    */
+    U32 usecs;                      /* Same thing in microseconds    */
+    U32 elapsed_usecs;              /* To calculate remaining time   */
+    int iswait;                     /* 1 == wait, 0 == pause         */
+    int rc;                         /* Return code from timed wait   */
+
+    /* Determine if pause statement or wait statement or neither  */
+    if (!strncasecmp( p, "pause ", 6 ))
+    {
+        p += 6;
+        iswait = 0;
+    }
+    else if (!strncasecmp( p, "wait ", 5 ))
+    {
+        p += 5;
+
+        if (fname)
+        {
+            // "Config file[%d] %s: Wait invalid for config files; pausing instead"
+            WRMSG( HHC02330, "W", *inc_stmtnum, fname );
+            iswait = 0;
+        }
+        else
+            iswait = 1;
+    }
+    else if (!strncasecmp( p, "*TestCase ", 10 ))
+    {
+        sysblk.scrtest = 1;
+        return 0;   /* Must be processed normally too */
+    }
+    else
+        return 0;   /* Neither pause nor wait nor TestCase */
+
+    /* Determine maximum pause or wait duration in seconds */
+#if defined( ENABLE_BUILTIN_SYMBOLS )
+    {
+        char *p2 = resolve_symbol_string( p );
+        if (p2)
+        {
+            secs = atof( p2 );
+            free( p2 );
+        }
+        else
+            secs = atof( p );
+    }
+#else
+    secs = atof( p );
+#endif
+
+    if (secs < 0.0 || secs > 999.0)
+    {
+        if (fname)
+            // "Config file[%d] %s: error processing statement: %s"
+            WRMSG( HHC01441, "W", *inc_stmtnum, fname, "syntax error; statement ignored" );
+        else
+            // "Script %d: file statement only; %s ignored"
+            WRMSG( HHC02261, "W", pCtl->scr_id, p );
+        return 1;   /* (go on to next statement)   */
+    }
+
+    /* Convert floating point seconds to other subsecond work values */
+    msecs = (U32) (secs * 1000.0); 
+    usecs = (msecs * 1000);
+
+    if (MLVL( VERBOSE ))
+    {
+        if (iswait)
+        {
+            // "Script %d: Waiting %d milliseconds for CPU(s) to enter disabled wait..."
+            WRMSG( HHC02331, "I", pCtl->scr_id, msecs );
+        }
+        else if (fname)
+        {
+            // "Config file[%d] %s: processing paused for %d milliseconds..."
+            WRMSG( HHC02318, "I", *inc_stmtnum, fname, msecs );
+        }
+        else
+        {
+            // "Script %d: processing paused for %d milliseconds..."
+            WRMSG( HHC02262, "I", pCtl->scr_id, msecs );
+        }
+    }
+
+    obtain_lock( &sysblk.scrlock );
+    ASSERT((!iswait && !sysblk.scrtest) || (iswait && sysblk.scrtest));
+    gettimeofday( &beg, NULL );
+    for (;;)
+    {
+        /* Check for cancelled script */
+        if (pCtl && script_abort( pCtl ))
+        {
+            sysblk.scrtest = 0;
+            release_lock( &sysblk.scrlock );
+            return 1;
+        }
+
+        /* Have all CPUs entered disabled wait yet? */
+        if (iswait && sysblk.scrtest > sysblk.cpus)
+        {
+            rc = 0;
+            break;
+        }
+
+        /* Not all CPUs are in a disabled wait yet */
+        /* Calculate how long to continue waiting  */
+        gettimeofday( &now, NULL );
+        timeval_subtract( &beg, &now, &dur );
+        elapsed_usecs = (dur.tv_sec * 1000000) + dur.tv_usec;
+
+        /* Is there any time remaining on the clock? */
+        if (elapsed_usecs >= (msecs * 1000))
+        {
+            rc = ETIMEDOUT;
+            break;
+        }
+
+        /* Wait for (another) CPU to go into disabled wait */
+        usecs = ((msecs * 1000) - elapsed_usecs);
+        timed_wait_condition_relative_usecs(
+            &sysblk.scrcond, &sysblk.scrlock, usecs, NULL );
+    }
+    sysblk.scrtest = 0;
+    release_lock( &sysblk.scrlock );
+
+    if (iswait && ETIMEDOUT == rc)
+    {
+        // HHC02332 "Script %d: Wait timeout"
+        WRMSG( HHC02332, "W", pCtl->scr_id );
+    }
+
+    /* Pause or wait completed */
+    if (MLVL( VERBOSE ))
+    {
+        if (fname)
+            // "Config file[%d] %s: processing resumed..."
+            WRMSG( HHC02319, "I", *inc_stmtnum, fname );
+        else
+            // "Script %d: processing resumed..."
+            WRMSG( HHC02263, "I", pCtl->scr_id );
+    }
+
+    return 1;  /* pause or wait complete */
+}
 
 #endif /*!defined(_GEN_ARCH)*/

tests/Makefile.am
@@ -199,4 +199,5 @@ EXTRA_DIST =            \
     tests.conf          \
     thder.txt           \
     trace.txt           \
-    trte.txt
+    trte.txt            \
+    wait.tst

tests/runtest
@@ -49,4 +49,5 @@ else
         ./hercules -f $dir/tests.conf -r $wfn.tst >$wfn.out
 fi
 
-rexx $dir/redtest.rexx $wfn.out
+sed -n -e "/^HHC01603I \*Testcase/,/^HHC01603I \*Done/p" > $wfn.sout $wfn.out
+rexx $dir/redtest.rexx $wfn.sout

tests/wait.tst
@@ -1,0 +1,209 @@
+
+msglvl -debug +verbose +emsgloc
+
+* ----------------------------------------------------------------------------
+*
+*Testcase Script 'wait' statement test
+*
+* ----------------------------------------------------------------------------
+*
+numcpu      4           #  Total CPUs needed for this test...
+*
+* ----------------------------------------------------------------------------
+*
+defsym  secs0     1     #  Loop duration in seconds for each CPU...
+defsym  secs1     2
+defsym  secs2     3
+defsym  secs3     4
+*
+defsym  wait     10.0   #  How long to 'wait' for test to finish
+*
+* ----------------------------------------------------------------------------
+*
+defsym  asec0     400   #  Address of "SECONDSn" fields...
+defsym  asec1     410
+defsym  asec2     420
+defsym  asec3     430
+*
+* ----------------------------------------------------------------------------
+*
+defsym  anow0     900   #  Address of "NOWTIMEn" and "ENDTIMEn" fields...
+defsym  aend0     910
+*
+defsym  anow1     920
+defsym  aend1     930
+*
+defsym  anow2     940
+defsym  aend2     950
+*
+defsym  anow3     960
+defsym  aend3     970
+*
+* ----------------------------------------------------------------------------
+* Must be contiguous!
+*
+defsym  flag0     a00   #  Address of "FLAGn" fields...
+defsym  flag1     a01
+defsym  flag2     a02
+defsym  flag3     a03
+*
+* ----------------------------------------------------------------------------
+*
+sysclear                #  Clear the world
+archmode z/Arch         #  Set z/Arch mode
+*
+r 1a0=0000000180000000  #  z/Arch RESTART PSW - part 1
+r 1a8=0000000000000200  #  z/Arch RESTART PSW - part 2 (address)
+*
+r 1d0=0002000180000000  #  z/Arch PGM NEW PSW - part 1
+r 1d8=00000000DEADDEAD  #  z/Arch PGM NEW PSW - part 2 (address)
+*
+* ----------------------------------------------------------------------------
+*
+r 200=1f00              #          SLR   R0,R0        Start clean
+r 202=41100001          #          LA    R1,1         Request z/Arch mode
+r 206=1f22              #          SLR   R2,R2        Start clean
+r 208=1f33              #          SLR   R3,R3        Start clean
+r 20a=ae020012          #          SIGP  R0,R2,X'12'  Request z/Arch mode
+r 20e=1f11              #          SLR   R1,R1        Start clean
+*
+r 210=41200000          #          LA    R2,0         Get our CPU number
+r 214=41400500          #          LA    R4,BEGIN0    Point to our loop
+r 218=404001ae          #          STH   R4,X'1AE'    Update restart PSW
+r 21c=ae020006          #          SIGP  R0,R2,X'6'   Restart our CPU
+r 220=b2b20310          #          LPSWE FAILPSW      How did we get here?!
+*
+* ----------------------------------------------------------------------------
+*
+r 300=0002000180000000  # GOODPSW  DC    0D'0',X'...  Success wait PSW part 1
+r 308=0000000000000000  #          DC    0D'0',X'...  Success wait PSW part 2
+r 310=0002000180000000  # FAILPSW  DC    0D'0',X'...  Failure wait PSW part 1
+r 318=00000000EEEEEEEE  #          DC    0D'0',X'...  Failure wait PSW part 2
+*
+* ----------------------------------------------------------------------------
+*
+r $(asec0)=0000000$(secs0)  # SECONDS0 DC    F'n'     Seconds CPU should loop
+r $(asec1)=0000000$(secs1)  # SECONDS1 DC    F'n'     Seconds CPU should loop
+r $(asec2)=0000000$(secs2)  # SECONDS2 DC    F'n'     Seconds CPU should loop
+r $(asec3)=0000000$(secs3)  # SECONDS3 DC    F'n'     Seconds CPU should loop
+*
+* ----------------------------------------------------------------------------
+*
+r 500=41200001          # BEGIN0   LA    R2,1         Get next CPU number
+r 504=41400600          #          LA    R4,BEGIN1    Point to next loop
+r 508=404001ae          #          STH   R4,X'1AE'    Update restart PSW
+r 50c=ae020006          #          SIGP  R0,R2,X'6'   Restart the next CPU
+*
+r 510=b2050$(aend0)     #          STCK     ENDTIME0  Current TOD clock value
+r 514=58100$(aend0)     #          L     R1,ENDTIME0  High word = approx. secs
+r 518=5e100$(asec0)     #          AL    R1,SECONDS0  Add seconds we're to loop
+r 51c=50100$(aend0)     #          ST    R1,ENDTIME0  Save ending clock value
+r 520=b2050$(anow0)     # LOOP0    STCK     NOWTIME0  Current TOD clock value
+r 524=58100$(anow0)     #          L     R1,NOWTIME0  High word = approx. secs
+r 528=55100$(aend0)     #          CL    R1,ENDTIME0  Reached ending TOD yet?
+r 52c=47400520          #          BL          LOOP0  Not yet. Keep looping...
+r 530=92ff0$(flag0)     #          MVI   X'FF',FLAG0  Indicate our loop ended
+r 534=b2b20300          #          LPSWE GOODPSW      Our CPU is now finished
+*
+* ----------------------------------------------------------------------------
+*
+r 600=41200002          # BEGIN1   LA    R2,2         Get next CPU number
+r 604=41400700          #          LA    R4,BEGIN2    Point to next loop
+r 608=404001ae          #          STH   R4,X'1AE'    Update restart PSW
+r 60c=ae020006          #          SIGP  R0,R2,X'6'   Restart the next CPU
+*  
+r 610=b2050$(aend1)     #          STCK     ENDTIME1  Current TOD clock value
+r 614=58100$(aend1)     #          L     R1,ENDTIME1  High word = approx. secs
+r 618=5e100$(asec1)     #          AL    R1,SECONDS1  Add seconds we're to loop
+r 61c=50100$(aend1)     #          ST    R1,ENDTIME1  Save ending clock value
+r 620=b2050$(anow1)     # LOOP1    STCK     NOWTIME1  Current TOD clock value
+r 624=58100$(anow1)     #          L     R1,NOWTIME1  High word = approx. secs
+r 628=55100$(aend1)     #          CL    R1,ENDTIME1  Reached ending TOD yet?
+r 62c=47400620          #          BL          LOOP1  Not yet. Keep looping...
+r 630=92ff0$(flag1)     #          MVI   X'FF',FLAG1  Indicate our loop ended
+r 634=b2b20300          #          LPSWE GOODPSW      Our CPU is now finished
+*
+* ----------------------------------------------------------------------------
+*
+r 700=41200003          # BEGIN2   LA    R2,3         Get next CPU number
+r 704=41400800          #          LA    R4,BEGIN3    Point to next loop
+r 708=404001ae          #          STH   R4,X'1AE'    Update restart PSW
+r 70c=ae020006          #          SIGP  R0,R2,X'6'   Restart the next CPU
+*  
+r 710=b2050$(aend2)     #          STCK     ENDTIME2  Current TOD clock value
+r 714=58100$(aend2)     #          L     R1,ENDTIME2  High word = approx. secs
+r 718=5e100$(asec2)     #          AL    R1,SECONDS2  Add seconds we're to loop
+r 71c=50100$(aend2)     #          ST    R1,ENDTIME2  Save ending clock value
+r 720=b2050$(anow2)     # LOOP2    STCK     NOWTIME2  Current TOD clock value
+r 724=58100$(anow2)     #          L     R1,NOWTIME2  High word = approx. secs
+r 728=55100$(aend2)     #          CL    R1,ENDTIME2  Reached ending TOD yet?
+r 72c=47400720          #          BL          LOOP2  Not yet. Keep looping...
+r 730=92ff0$(flag2)     #          MVI   X'FF',FLAG2  Indicate our loop ended
+r 734=b2b20300          #          LPSWE GOODPSW      Our CPU is now finished
+*
+* ----------------------------------------------------------------------------
+*
+r 800=b2050$(aend3)     # BEGIN3   STCK     ENDTIME3  Current TOD clock value
+r 804=58100$(aend3)     #          L     R1,ENDTIME3  High word = approx. secs
+r 808=5e100$(asec3)     #          AL    R1,SECONDS3  Add seconds we're to loop
+r 80c=50100$(aend3)     #          ST    R1,ENDTIME3  Save ending clock value
+r 810=b2050$(anow3)     # LOOP3    STCK     NOWTIME3  Current TOD clock value
+r 814=58100$(anow3)     #          L     R1,NOWTIME3  High word = approx. secs
+r 818=55100$(aend3)     #          CL    R1,ENDTIME3  Reached ending TOD yet?
+r 81c=47400810          #          BL          LOOP3  Not yet. Keep looping...
+r 820=92ff0$(flag3)     #          MVI   X'FF',FLAG3  Indicate our loop ended
+r 824=b2b20300          #          LPSWE GOODPSW      Our CPU is now finished
+*
+* ----------------------------------------------------------------------------
+*
+r $(anow0)=0123456789abcdef00000000  # NOWTIME0 DC    D'0',F'0'
+r $(aend0)=0123456789abcdef00000000  # ENDTIME0 DC    D'0',F'0'
+*
+r $(anow1)=0123456789abcdef00000001  # NOWTIME1 DC    D'0',F'1'
+r $(aend1)=0123456789abcdef00000001  # ENDTIME1 DC    D'0',F'1'
+*
+r $(anow2)=0123456789abcdef00000002  # NOWTIME2 DC    D'0',F'2'
+r $(aend2)=0123456789abcdef00000002  # ENDTIME2 DC    D'0',F'2'
+*
+r $(anow3)=0123456789abcdef00000003  # NOWTIME3 DC    D'0',F'3'
+r $(aend3)=0123456789abcdef00000003  # ENDTIME3 DC    D'0',F'3'
+*
+* ----------------------------------------------------------------------------
+* Must be contiguous!
+*
+r $(flag0)=f0           # FLAG0   DC   X'F0'           Silly test ended flag
+r $(flag1)=f1           # FLAG1   DC   X'F1'           Silly test ended flag
+r $(flag2)=f2           # FLAG2   DC   X'F2'           Silly test ended flag
+r $(flag3)=f3           # FLAG3   DC   X'F3'           Silly test ended flag
+*
+* ----------------------------------------------------------------------------
+*
+restart                 # Get the ball rolling...
+*
+wait  $(wait)           # Wait for CPUs to enter disabled wait
+*
+* ----------------------------------------------------------------------------
+*
+r $(anow0).c            # Show CPU 0 actual ending  TOD
+r $(aend0).c            # Show CPU 0 ending compare TOD
+*
+r $(anow1).c            # Show CPU 1 actual ending  TOD
+r $(aend1).c            # Show CPU 1 ending compare TOD
+*
+r $(anow2).c            # Show CPU 2 actual ending  TOD
+r $(aend2).c            # Show CPU 2 ending compare TOD
+*
+r $(anow3).c            # Show CPU 3 actual ending  TOD
+r $(aend3).c            # Show CPU 3 ending compare TOD
+*
+* ----------------------------------------------------------------------------
+*
+*Compare
+r $(flag0)-$(flag3)
+*Want FFFFFFFF
+*
+* ----------------------------------------------------------------------------
+*
+*Done
+*
+* ----------------------------------------------------------------------------