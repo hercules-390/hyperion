hercules-390/hyperion

commit e6ef572fc479754e9469afbd2be7bc18d28a6633

Helper functions to test if all CPUs are stopped:
  int are_all_cpus_stopped();
  int are_all_cpus_stopped_intlock_held();

Both are boolean functions returning TRUE or FALSE.

archlvl.c
@@ -646,8 +646,6 @@ BYTE als =
 
 int archlvl_cmd(int argc, char *argv[], char *cmdline)
 {
-    int i;
-
     UNREFERENCED(cmdline);
 
     if (argc < 2)
@@ -718,17 +716,12 @@ int archlvl_cmd(int argc, char *argv[], char *cmdline)
     }
 
     /* Make sure all CPUs are deconfigured or stopped */
-
-    OBTAIN_INTLOCK(NULL);
-    if(sysblk.cpus)
-        for(i = 0; i < sysblk.maxcpu; i++)
-            if(IS_CPU_ONLINE(i) && sysblk.regs[i]->cpustate == CPUSTATE_STARTED)
-            {
-                RELEASE_INTLOCK(NULL);
-                WRMSG( HHC02253, "E" );
-                return HERRCPUONL;
-            }
-    RELEASE_INTLOCK(NULL);
+    if (!are_all_cpus_stopped())
+    {
+        // "All CPU's must be stopped to change architecture"
+        WRMSG( HHC02253, "E" );
+        return HERRCPUONL;
+    }
 
     if(!set_archlvl(argv[1]))
         if(update_archlvl(argc, argv))

config.c
@@ -122,20 +122,8 @@ U32   skeysize;
 int cpu;
 
     /* Ensure all CPUs have been stopped */
-    if (sysblk.cpus)
-    {
-        OBTAIN_INTLOCK(NULL);
-        for (cpu = 0; cpu < sysblk.maxcpu; cpu++)
-        {
-            if (IS_CPU_ONLINE(cpu) &&
-                sysblk.regs[cpu]->cpustate == CPUSTATE_STARTED)
-            {
-                RELEASE_INTLOCK(NULL);
-                return HERRCPUONL;
-            }
-        }
-        RELEASE_INTLOCK(NULL);
-    }
+    if (!are_all_cpus_stopped())
+        return HERRCPUONL;
 
     /* Release storage and return if deconfiguring */
     if (mainsize == ~0ULL)
@@ -334,20 +322,8 @@ REGS *regs;
 int  cpu;
 
     /* Ensure all CPUs have been stopped */
-    if (sysblk.cpus)
-    {
-        OBTAIN_INTLOCK(NULL);
-        for (cpu = 0; cpu < sysblk.maxcpu; cpu++)
-        {
-            if (IS_CPU_ONLINE(cpu) &&
-                sysblk.regs[cpu]->cpustate == CPUSTATE_STARTED)
-            {
-                RELEASE_INTLOCK(NULL);
-                return HERRCPUONL;
-            }
-        }
-        RELEASE_INTLOCK(NULL);
-    }
+    if (!are_all_cpus_stopped())
+        return HERRCPUONL;
 
     /* Release storage and return if zero or deconfiguring */
     if (!xpndsize ||

hexterns.h
@@ -559,6 +559,8 @@ CCDU_DLL_IMPORT void    cckdumsg (DEVBLK *, int, char *, ...) ATTR_PRINTF(3,4);
 /* Functions in module hscmisc.c */
 int herc_system (char* command);
 void do_shutdown();
+int are_all_cpus_stopped();
+int are_all_cpus_stopped_intlock_held();
 int display_gregs (REGS *regs, char *buf, int buflen, char *hdr);
 int display_fregs (REGS *regs, char *buf, int buflen,char *hdr);
 int display_cregs (REGS *regs, char *buf, int buflen, char *hdr);

hscmisc.c
@@ -28,6 +28,38 @@
 #define _HSCMISC_C
 
 /*-------------------------------------------------------------------*/
+/* Test if all CPUs are in stopped state      (INTLOCK held)         */
+/*-------------------------------------------------------------------*/
+int are_all_cpus_stopped_intlock_held()
+{
+int cpu;
+
+    if (sysblk.cpus)
+        for (cpu = 0; cpu < sysblk.maxcpu; cpu++)
+            if (IS_CPU_ONLINE( cpu ) &&
+                sysblk.regs[ cpu ]->cpustate != CPUSTATE_STOPPED)
+                return FALSE;
+    return TRUE;
+}
+
+
+/*-------------------------------------------------------------------*/
+/* Test if all CPUs are in stopped state      (INTLOCK not held)     */
+/*-------------------------------------------------------------------*/
+int are_all_cpus_stopped()
+{
+int all_stopped;
+
+    OBTAIN_INTLOCK( NULL );
+    {
+        all_stopped = are_all_cpus_stopped_intlock_held();
+    }
+    RELEASE_INTLOCK( NULL );
+    return all_stopped;
+}
+
+
+/*-------------------------------------------------------------------*/
 /*                System Shutdown Processing                         */
 /*-------------------------------------------------------------------*/
 
@@ -52,15 +84,13 @@ int pending;
 
 static void wait_sigq_resp()
 {
-int pending, i;
+int pending;
     /* Wait for all CPU's to stop */
     do
     {
         OBTAIN_INTLOCK(NULL);
         wait_sigq_pending = 0;
-        for (i = 0; i < sysblk.maxcpu; i++)
-        if (IS_CPU_ONLINE(i)
-          && sysblk.regs[i]->cpustate != CPUSTATE_STOPPED)
+        if (!are_all_cpus_stopped_intlock_held())
             wait_sigq_pending = 1;
         pending = wait_sigq_pending;
         RELEASE_INTLOCK(NULL);

hscpufun.c
@@ -1060,7 +1060,7 @@ char   *archlvl_func;
 /*-------------------------------------------------------------------*/
 int cmpscpad_cmd( int argc, char* argv[], char* cmdline )
 {
-    int bits, i;
+    int bits;
     const char* ptr;
     char* nxt;
     char buf[8];
@@ -1078,19 +1078,12 @@ int cmpscpad_cmd( int argc, char* argv[], char* cmdline )
 
     OBTAIN_INTLOCK( NULL );
 
-    if (sysblk.cpus)
+    if (!are_all_cpus_stopped_intlock_held())
     {
-        for (i = 0; i < sysblk.maxcpu; i++)
-        {
-            if (IS_CPU_ONLINE( i ) &&
-                sysblk.regs[ i ]->cpustate == CPUSTATE_STARTED)
-            {
-                RELEASE_INTLOCK( NULL );
-                // "CPUs must be offline or stopped"
-                WRMSG( HHC02389, "E" );
-                return HERRCPUONL;
-            }
-        }
+        RELEASE_INTLOCK( NULL );
+        // "CPUs must be offline or stopped"
+        WRMSG( HHC02389, "E" );
+        return HERRCPUONL;
     }
 
     if ( argc == 2 )

hsccmd.c
@@ -200,22 +200,6 @@ static inline void missing_devnum()
     WRMSG(HHC02201,"E");
 }
 
-#if 0
-dead code
-/* Check for all processors stopped */
-#define ALL_STOPPED all_stopped()
-static inline int all_stopped()
-{
-    int   i;
-
-    for ( i = 0; i < sysblk.maxcpu; i++)
-        if ( IS_CPU_ONLINE(i) &&
-             sysblk.regs[i]->cpustate != CPUSTATE_STOPPED )
-            return 0;
-    return 1;
-}
-#endif
-
 
 /**********************************************************************/
 /*                                                                    */

hscemode.c
@@ -30,22 +30,6 @@ static inline void missing_devnum()
     WRMSG(HHC02201,"E");
 }
 
-#if 0
-dead code and a copy to boot
-/* Check for all processors stopped */
-#define ALL_STOPPED all_stopped()
-static inline int all_stopped()
-{
-    int   i;
-
-    for ( i = 0; i < sysblk.maxcpu; i++)
-        if ( IS_CPU_ONLINE(i) &&
-             sysblk.regs[i]->cpustate != CPUSTATE_STOPPED )
-            return 0;
-    return 1;
-}
-#endif
-
 
 static inline char *aea_mode_str(BYTE mode)
 {

sr.c
@@ -402,17 +402,12 @@ int      numconfdev=0;
 
     /* Make sure all CPUs are deconfigured or stopped */
     TRACE("SR: Waiting for CPUs to stop...\n");
-    OBTAIN_INTLOCK(NULL);
-    for (i = 0; i < sysblk.maxcpu; i++)
-        if (IS_CPU_ONLINE(i)
-         && CPUSTATE_STOPPED != sysblk.regs[i]->cpustate)
-        {
-            RELEASE_INTLOCK(NULL);
-            // "SR: all processors must be stopped to resume"
-            WRMSG(HHC02005, "E");
-            return -1;
-        }
-    RELEASE_INTLOCK(NULL);
+    if (!are_all_cpus_stopped())
+    {
+        // "SR: all processors must be stopped to resume"
+        WRMSG(HHC02005, "E");
+        return -1;
+    }
 
     file = SR_OPEN (fn, "rb");
     if (file == NULL)

commit f425c5346f32c09263092a86fd8a0dfd3e370286

Fix "CPUs must be offline or stopped":
Fixes bug introduced by commit e6ef572fc479754e9469afbd2be7bc18d28a6633.

archlvl.c
@@ -716,7 +716,7 @@ int archlvl_cmd(int argc, char *argv[], char *cmdline)
     }
 
     /* Make sure all CPUs are deconfigured or stopped */
-    if (!are_all_cpus_stopped())
+    if (are_any_cpus_started())
     {
         // "All CPU's must be stopped to change architecture"
         WRMSG( HHC02253, "E" );

config.c
@@ -122,7 +122,7 @@ U32   skeysize;
 int cpu;
 
     /* Ensure all CPUs have been stopped */
-    if (!are_all_cpus_stopped())
+    if (are_any_cpus_started())
         return HERRCPUONL;
 
     /* Release storage and return if deconfiguring */
@@ -322,7 +322,7 @@ REGS *regs;
 int  cpu;
 
     /* Ensure all CPUs have been stopped */
-    if (!are_all_cpus_stopped())
+    if (are_any_cpus_started())
         return HERRCPUONL;
 
     /* Release storage and return if zero or deconfiguring */

hexterns.h
@@ -561,6 +561,8 @@ int herc_system (char* command);
 void do_shutdown();
 int are_all_cpus_stopped();
 int are_all_cpus_stopped_intlock_held();
+int are_any_cpus_started();
+int are_any_cpus_started_intlock_held();
 int display_gregs (REGS *regs, char *buf, int buflen, char *hdr);
 int display_fregs (REGS *regs, char *buf, int buflen,char *hdr);
 int display_cregs (REGS *regs, char *buf, int buflen, char *hdr);

hscmisc.c
@@ -28,6 +28,22 @@
 #define _HSCMISC_C
 
 /*-------------------------------------------------------------------*/
+/* Test if any CPUs are in started state      (INTLOCK held)         */
+/*-------------------------------------------------------------------*/
+int are_any_cpus_started_intlock_held()
+{
+int cpu;
+
+    if (sysblk.cpus)
+        for (cpu = 0; cpu < sysblk.maxcpu; cpu++)
+            if (IS_CPU_ONLINE( cpu ) &&
+                sysblk.regs[ cpu ]->cpustate == CPUSTATE_STARTED)
+                return TRUE;
+    return FALSE;
+}
+
+
+/*-------------------------------------------------------------------*/
 /* Test if all CPUs are in stopped state      (INTLOCK held)         */
 /*-------------------------------------------------------------------*/
 int are_all_cpus_stopped_intlock_held()
@@ -40,6 +56,22 @@ int cpu;
                 sysblk.regs[ cpu ]->cpustate != CPUSTATE_STOPPED)
                 return FALSE;
     return TRUE;
+}
+
+
+/*-------------------------------------------------------------------*/
+/* Test if any CPUs are in started state      (INTLOCK not held)     */
+/*-------------------------------------------------------------------*/
+int are_any_cpus_started()
+{
+int any_started;
+
+    OBTAIN_INTLOCK( NULL );
+    {
+        any_started = are_any_cpus_started_intlock_held();
+    }
+    RELEASE_INTLOCK( NULL );
+    return any_started;
 }
 
 

hscpufun.c
@@ -1078,7 +1078,7 @@ int cmpscpad_cmd( int argc, char* argv[], char* cmdline )
 
     OBTAIN_INTLOCK( NULL );
 
-    if (!are_all_cpus_stopped_intlock_held())
+    if (are_any_cpus_started_intlock_held())
     {
         RELEASE_INTLOCK( NULL );
         // "CPUs must be offline or stopped"