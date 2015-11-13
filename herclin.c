hercules-390/hyperion

commit 080b725a4301d9e7964c53b95a5158adebf6b458

General cleanup, mostly related to log message handling:
Removed  OPTION_LOCK_CONFIG_FILE (not needed and implemented wrong too)
Removed  OPTION_CMDTGT, OPTION_MSGCLR, OPTION_MSGHLD, OPTION_MSGLCK
         (Disabled by Jan since 10 Sep 2011 because not thread safe)

DISPLAY_INSTRUCTION_OPERANDS made permanent
OPTION_MVS_TELNET_WORKAROUND introduced

Fixed (all?) non-standard messages
Fixed instruction trace messages
Fixed herclin utility to build with pch (precompiled headers)
Fixed logger log_read bug
Fixed herclin so that it now actually works as expected
      (double messages fixed, Hercules exit detected, MSVC-only code removed)

MSGLVL 'debug' now works again (source file and line number prefix)
MSGLVL 'emsgloc' option identifies where error messages were issued.

Standardized and simplified utility startup
Standardized utility messages (all now use WRMSG or FWRMSG)
Standardized version reporting (version, copyright, AND build date)
Separated version and build-options reporting functions

Deleted some #if 0 disabled code
Activated some #if 1 enabled code

herclin.c
@@ -16,59 +16,38 @@
 /* INTERFACE FEATURE.                           */
 /************************************************/
 
-#if 1 // fish test: we SHOULD be doing it this way! Why aren't we?!
-
-  #include "hstdinc.h"
-  #include "hercules.h"
-
-#else
-
-  #ifdef _MSVC_
-  #include <windows.h>
-  #include <conio.h>
-  #endif
-  #ifdef HAVE_CONFIG_H
-    #include <config.h> // Hercules build configuration options/settings
-  #endif
-  #include <stdio.h>
-  #include <stdlib.h>
-  #include <string.h>
-  #include <sys/mman.h>
-
-#endif
+#include "hstdinc.h"
+#include "hercules.h"
 #include "hextapi.h"
 
 /**********************************************/
 /* The following function is the LOG callback */
 /* function. It gets called by the engine     */
 /* whenever a log message needs to be         */
-/* displayed. This function may therefore be  */
-/* invoked from a separate thread             */
-/*                                            */
-/* This simple version simply writes to STDOUT*/
+/* displayed. This function therefore         */
+/* might be invoked from a separate thread    */
 /**********************************************/
 
-#ifdef _MSVC_
-static HANDLE hLogCallbackThread = NULL;
-#endif
+int hercules_has_exited = 0;
 
-void mywrite(const char *a,size_t b)
+void log_callback( const char* msg, size_t msglen )
 {
-#ifdef _MSVC_
-    if (!hLogCallbackThread)
-        hLogCallbackThread = OpenThread(
-            SYNCHRONIZE, FALSE,
-            GetCurrentThreadId());
-#endif
+    UNREFERENCED( msg );
 
-    fflush(stdout);
-    fwrite(a,b,1,stdout);
-    fflush(stdout);
+    if (!msglen)
+    {
+        hercules_has_exited = 1;
+        return;
+    }
+
+    fflush( stdout );
+    fflush( stderr );
 }
 
-
-int main(int ac,char **av)
+int main( int argc, char* argv[] )
 {
+    int rc;
+
     /*****************************************/
     /* COMMANDHANDLER is the function type   */
     /* of the engine's panel command handler */
@@ -77,8 +56,9 @@ int main(int ac,char **av)
     /* redirected the initial engine function*/
     /*****************************************/
 
-    COMMANDHANDLER  ch;
-    char *str,*bfr;
+    COMMANDHANDLER  process_command;
+
+    char *cmdbuf, *cmd;
 
 #if defined( OPTION_DYNAMIC_LOAD ) && defined( HDL_USE_LIBTOOL )
     /* LTDL Preloaded symbols for HDL using libtool */
@@ -86,49 +66,40 @@ int main(int ac,char **av)
 #endif
 
     /******************************************/
-    /* Register the 'mywrite' function as the */
-    /* log callback routine                   */
+    /* Register the 'log_callback' function   */
+    /* as the log message callback routine    */
     /******************************************/
-    registerLogCallback(mywrite);
+    registerLogCallback( log_callback );
 
     /******************************************/
     /* Initialize the HERCULE Engine          */
     /******************************************/
-    impl(ac,av);
-
-    /******************************************/
-    /* Get the command handler function       */
-    /* This MUST be done after IML            */
-    /******************************************/
-    ch=getCommandHandler();
-
-    /******************************************/
-    /* Read STDIN and pass to Command Handler */
-    /******************************************/
-    bfr=(char *)malloc(1024);
-    while
-    (
-#ifdef _MSVC_
-        !hLogCallbackThread ||
-        WaitForSingleObject(hLogCallbackThread,0)
-            != WAIT_OBJECT_0
-#else
-        1
-#endif
-    )
+    if ((rc = impl( argc, argv )) == 0)
     {
-#ifdef _MSVC_
-        if (!kbhit()) Sleep(50); else
-#endif
-        if ((str=fgets(bfr,1024,stdin)))
-        {
-            str[strlen(str)-1]=0;
-            ch(str);
-        }
-    }
-#ifdef _MSVC_
-    CloseHandle(hLogCallbackThread);
-#endif
-    UNREACHABLE_CODE();
-}
+        /******************************************/
+        /* Get the command handler function       */
+        /* This MUST be done after IML            */
+        /******************************************/
+        process_command = getCommandHandler();
 
+        /******************************************/
+        /* Read STDIN and pass to Command Handler */
+        /******************************************/
+#define CMDBUFSIZ  1024
+        cmdbuf = (char*) malloc( CMDBUFSIZ );
+
+        do
+        {
+            if ((cmd = fgets( cmdbuf, CMDBUFSIZ, stdin )))
+            {
+                /* (remove newline) */
+                cmd[ strlen( cmd ) - 1 ] = 0;
+
+                process_command( cmd );
+            }
+        }
+        while (!hercules_has_exited);
+    }
+
+    return rc;
+}

commit a40e0bc802a9d4fb16ba8c9eeee4bc9c5fc9df50

Fix herclin accidentally broken by commit 080b725a4301d9e7964c53b95a5158adebf6b458

herclin.c
@@ -41,6 +41,8 @@ void log_callback( const char* msg, size_t msglen )
     }
 
     fflush( stdout );
+    fwrite( msg, 1, msglen, stdout );
+    fflush( stdout );
     fflush( stderr );
 }
 