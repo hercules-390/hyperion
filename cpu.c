hercules-390/hyperion

commit 2c03ad1ad93afaa65fa5196085a6162ffcd13e51

Windows 'runtest' tweaks:
1. Add support for runtest script command arguments.
2. Tweak end of test detection in cpu.c and script.c
3. Split runtest.tst test into runtest0.tst and runtest4.tst
4. Add tests recently added by previous commits to makefile.am and vcproj.
5. Document runtest test script command in readme.

cpu.c
@@ -1459,12 +1459,19 @@ CPU_Wait (REGS* regs)
         wait_condition (&sysblk.sync_bc_cond, &sysblk.intlock);
     }
 
+#if defined(_MSVC_)
+        /* Let waiting script know all CPUs are now STOPPED */
+        obtain_lock( &sysblk.scrlock );
+        if (sysblk.scrtest && !sysblk.started_mask)
+        {
+            sysblk.scrtest++;
+            broadcast_condition( &sysblk.scrcond );
+        }
+        release_lock( &sysblk.scrlock );
+#else
     /* Quick check to test if lock must be obtainied.  This is valid */
     /* because  the  system  block pointer is set only when all CPUs */
     /* are stopped.                                                  */
-#if defined(_MSVC_)
-    /* FORFISH                                                       */
-#else
     if (sysblk.scrsem && !sysblk.started_mask)
     {
        /* Looks  like we are entering into a state where nothing can */
@@ -1643,19 +1650,6 @@ cpustate_stopping:
         TOD saved_timer = cpu_timer(regs);
         regs->ints_state = IC_INITIAL_STATE;
         sysblk.started_mask ^= regs->cpubit;
-
-        /* Let waiting script know CPU is now STOPPED. Please
-           note that when a CPU loads a disabled wait state PSW
-           it sets its state to STOPPED as well, and eventually
-           reaches here to actually stop ('CPU_Wait' function).
-        */
-        obtain_lock( &sysblk.scrlock );
-        if (sysblk.scrtest)
-        {
-            sysblk.scrtest++;
-            broadcast_condition( &sysblk.scrcond );
-        }
-        release_lock( &sysblk.scrlock );
 
         CPU_Wait(regs);
 

script.c
@@ -391,11 +391,7 @@ int     shell_flg = FALSE;              /* indicate it is has a shell
 #endif /* #if defined( ENABLE_CONFIG_INCLUDE ) */
 
         if ( ( strlen(addargv[0]) <= 4 &&
-#if defined( _MSVC_)
-               sscanf_s(addargv[0], "%x%c", &rc, &c, sizeof(BYTE)) == 1)
-#else
-               sscanf(addargv[0], "%x%c", &rc, &c) == 1 )
-#endif
+               sscanf(addargv[0], "%"SCNx32"%c", &rc, &c) == 1 )
              ||
         /* Also, if addargv[0] contains ':' (added by Harold Grovesteen jan2008)  */
         /* Added because device statements may now contain channel set or LCSS id */
@@ -450,11 +446,7 @@ int     shell_flg = FALSE;              /* indicate it is has a shell
 
         /* Check for old-style CPU statement */
         if (scount == 0 && addargc == 7 && strlen(addargv[0]) == 6
-#if defined( _MSVC_)
-            && sscanf_s(addargv[0], "%x%c", &rc, &c, sizeof(BYTE)) == 1)
-#else
-            && sscanf(addargv[0], "%x%c", &rc, &c) == 1)
-#endif
+            && sscanf(addargv[0], "%"SCNx32"%c", &rc, &c) == 1)
         {
         char *exec_cpuserial[2] = { "cpuserial", NULL };
         char *exec_cpumodel[2]  = { "cpumodel", NULL };
@@ -1106,38 +1098,10 @@ int $runtest_cmd(int argc,char *argv[], char *cmdline)
 /*-------------------------------------------------------------------*/
 static int test_abort( SCRCTL *pCtl )
 {
-    // TODO: decide how test abort should be handled: abort() the
-    // process, issue severity 'S' message and exit Hercules, or
-    // issue 'E' severity message and continue to let redtest.rexx
-    // test results parser script detect/report the failure, CRASH,
-    // etc.
-
     // "Script %d: test: aborted"
     WRMSG( HHC02331, "E", pCtl->scr_id );
-
-#if 0
-    CRASH();
-#elif 0
-    abort();
-#endif
-
+    panel_command( "sysclear");
     return -1;
-}
-
-/*-------------------------------------------------------------------*/
-/*                       is_test_done                                */
-/*-------------------------------------------------------------------*/
-static int is_test_done()
-{
-    /* If all CPUs are now in the STOPPED state then we are done.
-       Note that when a CPU loads a disabled wait state PSW it also
-       sets its state to STOPPED afterwards. See process_interrupt
-       in source module cpu.c
-    */
-    if (sysblk.scrtest > sysblk.cpus)
-        return TRUE;
-
-    return FALSE;   /* runtest has not completed yet */
 }
 
 /*-------------------------------------------------------------------*/
@@ -1145,44 +1109,75 @@ static int is_test_done()
 /*-------------------------------------------------------------------*/
 int runtest( SCRCTL *pCtl, char *cmdline, char *args )
 {
+    char* p2 = NULL;                /* Resolved symbol string        */
     struct timeval beg, now, dur;   /* To calculate remaining time   */
-    double secs;                    /* Optional timeout in seconds   */
+    double secs = DEF_RUNTEST_DUR;  /* Optional timeout in seconds   */
     U32 usecs;                      /* Same thing in microseconds    */
     U32 sleep_usecs;                /* Remaining microseconds        */
     U32 elapsed_usecs;              /* To calculate remaining time   */
     int rc;                         /* Return code from timed wait   */
-
+    int dostart = 0;                /* 0 = start test via "restart"  */
+                                    /* 1 = start test via "start"    */
     UNREFERENCED( cmdline );
 
     ASSERT( sysblk.scrtest );       /* How else did we get called?   */
 
+    /* Parse optional RUNTEST command arguments. */
+    /* Syntax: RUNTEST [RESTART|START] [timeout] */
+
     if (*args && *args != '#')
     {
-        /* Parse the optional timeout value */
 #if defined( ENABLE_BUILTIN_SYMBOLS )
+        p2 = resolve_symbol_string( args );
+        if (p2)
+            args = p2;
+#endif
+
+        if (isalpha( args[0] ))  /* [RESTART|START]? */
         {
-            char *p2 = resolve_symbol_string( args );
-            if (p2)
+#define MAX_KW_LEN 15
+            char kw[ MAX_KW_LEN + 1 ] = {0};
+            char* pkw = NULL;
+
+            if (sscanf( args, "%"QSTR( MAX_KW_LEN )"s %lf", kw, &secs ))
             {
-                secs = atof( p2 );
-                free( p2 );
+                if (strcasecmp( kw, "start" ) == 0)
+                    dostart = 1;
+                else if (strcasecmp( kw, "restart" ) != 0)
+                    pkw = kw;
             }
             else
-                secs = atof( args );
-        }
-#else
-        secs = atof( args );
-#endif
-    }
-    else
-        secs = DEF_RUNTEST_TIMEOUT;
+                pkw = args;
 
-    /* Validate optional timeout value */
-    if (secs < MIN_RUNTEST_TIMEOUT || secs > MAX_RUNTEST_TIMEOUT)
-    {
-        // "Script %d: test: invalid timeout; set to def: %s"
-        WRMSG( HHC02335, "W", pCtl->scr_id, args );
-        secs = DEF_RUNTEST_TIMEOUT;
+            if (pkw)
+            {
+                // "Script %d: test: unknown runtest keyword: %s"
+                WRMSG( HHC02341, "E", pCtl->scr_id, pkw );
+                if (p2)
+                    free( p2 );
+                return test_abort( pCtl );
+            }
+
+            /* Get past keyword to next argument */
+            args += strlen( kw );
+        }
+
+        if ( args[0] ) /* [timeout]? */
+        {
+            if (0
+                || sscanf( args, "%lf", &secs ) < 1
+                || secs < MIN_RUNTEST_DUR
+                || secs > MAX_RUNTEST_DUR
+            )
+            {
+                // "Script %d: test: invalid timeout; set to def: %s"
+                WRMSG( HHC02335, "W", pCtl->scr_id, args );
+                secs = DEF_RUNTEST_DUR;
+            }
+        }
+
+        if (p2)
+            free( p2 );
     }
 
     /* Apply adjustment factor */
@@ -1201,15 +1196,20 @@ int runtest( SCRCTL *pCtl, char *cmdline, char *args )
                                             usecs % 1000000 );
     }
 
-    /* Press the restart button to start the test. NOTE: the
-       restart command itself does not require any arguments. */
+    /* Press the restart or start button to start the test */
 
-    sysblk.scrtest = 1; // (reset)
-    rc = restart_cmd( 0, NULL, NULL );
+    obtain_lock( &sysblk.scrlock );
+    sysblk.scrtest = 1;
+    release_lock( &sysblk.scrlock );
+
+    if (dostart)
+        rc = start_cmd_cpu( 0, NULL, NULL );
+    else
+        rc = restart_cmd( 0, NULL, NULL );
 
     if (rc)
     {
-        // "Script %d: test: restart failed"
+        // "Script %d: test: [re]start failed"
         WRMSG( HHC02330, "E", pCtl->scr_id );
         return test_abort( pCtl );
     }
@@ -1230,7 +1230,7 @@ int runtest( SCRCTL *pCtl, char *cmdline, char *args )
         }
 
         /* Has the test finished yet? */
-        if (is_test_done())
+        if (sysblk.scrtest > 1)
         {
             rc = 0;
             break;
@@ -1260,7 +1260,7 @@ int runtest( SCRCTL *pCtl, char *cmdline, char *args )
 
     if (ETIMEDOUT == rc)
     {
-        // HHC02332 "Script %d: test: timeout"
+        // "Script %d: test: timeout"
         WRMSG( HHC02332, "E", pCtl->scr_id );
         return test_abort( pCtl );
     }
@@ -1349,7 +1349,6 @@ do_special(char *fname, int *inc_stmtnum, SCRCTL *pCtl, char *p)
 
     /* Convert floating point seconds to other subsecond work values */
     msecs = (U32) (secs * 1000.0);
-    usecs = (msecs * 1000);
 
     if (MLVL( VERBOSE ))
     {
@@ -1418,7 +1417,7 @@ proc_runtest(SCRCTL *pCtl, char *args)
 {
    char * p2 = NULL;
    int rv;
-   double secs = DEF_RUNTEST_TIMEOUT; /* 30 secons, perhaps          */
+   double secs = DEF_RUNTEST_DUR;     /* 30 secons, perhaps          */
    int dostart = 0;                /* Assume fire program by restart */
    sem_t sem;
    struct timespec ts, target;
@@ -1448,11 +1447,11 @@ proc_runtest(SCRCTL *pCtl, char *args)
 
    if (p2) free(p2);
 
-   if (secs < MIN_RUNTEST_TIMEOUT || secs > MAX_RUNTEST_TIMEOUT)
+   if (secs < MIN_RUNTEST_DUR || secs > MAX_RUNTEST_DUR)
    {
       // "Script %d: test: invalid timeout; set to def: %s"
       WRMSG( HHC02335, "W", pCtl->scr_id, args );
-      secs = DEF_RUNTEST_TIMEOUT;
+      secs = DEF_RUNTEST_DUR;
    }
 
    ts.tv_sec = secs;

Hercules.vcproj
@@ -3006,6 +3006,14 @@
 								>
 							</File>
 							<File
+								RelativePath=".\tests\invpsw.assemble"
+								>
+							</File>
+							<File
+								RelativePath=".\tests\leapfrog.assemble"
+								>
+							</File>
+							<File
 								RelativePath=".\tests\logicimm.assemble"
 								>
 							</File>
@@ -3062,6 +3070,14 @@
 								>
 							</File>
 							<File
+								RelativePath=".\tests\invpsw.tst"
+								>
+							</File>
+							<File
+								RelativePath=".\tests\leapfrog.tst"
+								>
+							</File>
+							<File
 								RelativePath=".\tests\logicimm.tst"
 								>
 							</File>
@@ -3074,7 +3090,11 @@
 								>
 							</File>
 							<File
-								RelativePath=".\tests\runtest.tst"
+								RelativePath=".\tests\runtest0.tst"
+								>
+							</File>
+							<File
+								RelativePath=".\tests\runtest4.tst"
 								>
 							</File>
 							<File
@@ -3097,6 +3117,10 @@
 								RelativePath=".\tests\sske390.tst"
 								>
 							</File>
+							<File
+								RelativePath=".\tests\timeout.tst"
+								>
+							</File>
 						</Filter>
 						<Filter
 							Name="listing"
@@ -3107,6 +3131,14 @@
 							</File>
 							<File
 								RelativePath=".\tests\ilc.listing"
+								>
+							</File>
+							<File
+								RelativePath=".\tests\invpsw.listing"
+								>
+							</File>
+							<File
+								RelativePath=".\tests\leapfrog.listing"
 								>
 							</File>
 							<File

hconsts.h
@@ -350,11 +350,11 @@
 #define DEF_PAUSE_TIMEOUT         1.0   /* Default pause seconds     */
 #define MAX_PAUSE_TIMEOUT       999.0   /* Maximum pause seconds     */
 
-#define MIN_RUNTEST_TIMEOUT     0.001   /* Minimum runtest seconds   */
-#define DEF_RUNTEST_TIMEOUT      30.0   /* Default runtest seconds   */
-#define MAX_RUNTEST_TIMEOUT     300.0   /* Maximum runtest seconds   */
+#define MIN_RUNTEST_DUR         0.001   /* Minimum runtest seconds   */
+#define DEF_RUNTEST_DUR          30.0   /* Default runtest seconds   */
+#define MAX_RUNTEST_DUR         300.0   /* Maximum runtest seconds   */
 
 #define MAX_RUNTEST_FACTOR  (((4.0 * 1024.0 * 1024.0 * 1024.0) - 1.0) \
                             / 1000000.0 /* (usecs) */                 \
-                            / MAX_RUNTEST_TIMEOUT)
+                            / MAX_RUNTEST_DUR)
 #endif // _HCONSTS_H

msgenu.h
@@ -1400,6 +1400,7 @@ Examples:
 #define HHC02338 "Script %d: test: actual duration: %"PRId32".%06"PRId32" seconds"
 #define HHC02339 "Script %d: test: duration limit: %"PRId32".%06"PRId32" seconds"
 #define HHC02340 "Script %d: test: cannot initalise semaphore: %s"
+#define HHC02341 "Script %d: test: unknown runtest keyword: %s"
 
 // range 02350 - 02369 available
 

tests/Makefile.am
@@ -53,6 +53,9 @@ EXTRA_DIST =            \
     ilc.assemble        \
     ilc.listing         \
     ilc.tst             \
+    invpsw.assemble     \
+    invpsw.listing      \
+    invpsw.tst          \
     kimd0.txt           \
     kimd1.txt           \
     kimd2.txt           \
@@ -147,6 +150,9 @@ EXTRA_DIST =            \
     kmo9.txt            \
     ldetr.txt           \
     ldxtr.txt           \
+    leapfrog.assemble   \
+    leapfrog.listing    \
+    leapfrog.tst        \
     ledtr.txt           \
     lfpc.txt            \
     llhrl.txt           \
@@ -177,6 +183,8 @@ EXTRA_DIST =            \
     rrxtr.txt           \
     runtest             \
     runtest.cmd         \
+    runtest0.tst        \
+    runtest4.tst        \
     srdt.txt            \
     sske.assemble       \
     sske.listing        \
@@ -198,6 +206,6 @@ EXTRA_DIST =            \
     tdgdt.txt           \
     tests.conf          \
     thder.txt           \
+    timeout.tst         \
     trace.txt           \
-    trte.txt            \
-    wait.tst
+    trte.txt

tests/README
@@ -120,3 +120,34 @@ are case sensitive. Using "*testcase" for example accomplishes nothing.
 logicimm.tst shows examples of all directives, except *Key, *Prefix, and
 *Program;  see sske.tst for an example of *Key and *program.  See pr.tst
 for *Prefix.
+
+.tst file RUNTEST command
+-------------------------
+
+The test a .tst script defines is normally begun via a special 'runtest'
+script command which supports optional arguments indicating how the test
+should be started as well as the maximum amount of time it should run:
+
+      RUNTEST   [RESTART|START]   [timeout]   [# comment...]
+
+The first argument must be either "restart" or "start" (or unspecified)
+and indicates how the test should be started: via the Hercules "restart"
+console command or the "start" console command. The default is to start
+the test via the "restart" command.
+
+The second argument (which is also optional) defines the maximum amount
+of time (specified in whole or fractional number of seconds) that the
+test is allowed to run. This is a safety feature to prevent runaway tests
+from running forever due to a bug in the test or within Hercules itself.
+(Under normal circumstances all tests should end automatically the very
+moment they are done.) The default if not specified is 30 seconds.
+
+Please also note that timeout values are multiplied by the test timeout
+factor value which specified on the command line before being used. The
+test timeout factor value is used to allow for running tests on systems
+that may be slower than the system they were originally designed for.
+Refer to the documentation describing Hercules's supported command line
+arguments.
+
+Other runtest and/or command line arguments may be added in the future
+as they are needed.

tests/runtest4.tst
@@ -3,19 +3,19 @@ msglvl +verbose +emsgloc
 msglvl +time +usecs
 
 * ----------------------------------------------------------------------------
-*Testcase runtest#1: all cpus 0 seconds
+*Testcase runtest4: all cpus 1-4 seconds
 * ----------------------------------------------------------------------------
 *
 numcpu      4           #  Total CPUs needed for this test...
 *
 * ----------------------------------------------------------------------------
 *
-defsym  secs0     0     #  Loop duration in seconds for each CPU...
-defsym  secs1     0
-defsym  secs2     0
-defsym  secs3     0
+defsym  secs0     1     #  Loop duration in seconds for each CPU...
+defsym  secs1     2
+defsym  secs2     3
+defsym  secs3     4
 *
-defsym  maxdur   0.4    #  Expected test duration, give or take.
+defsym  maxdur   4.5    #  Expected test duration, give or take.
 *
 * ----------------------------------------------------------------------------
 *
@@ -45,175 +45,6 @@ defsym  flag0     a00   #  Address of "FLAGn" fields...
 defsym  flag1     a01
 defsym  flag2     a02
 defsym  flag3     a03
-*
-* ----------------------------------------------------------------------------
-*
-sysclear                #  Clear the world
-archmode z/Arch         #  Set z/Arch mode
-*
-r 1a0=0000000180000000  #  z/Arch RESTART PSW - part 1
-r 1a8=0000000000000200  #  z/Arch RESTART PSW - part 2 (address)
-*
-r 1d0=0002000180000000  #  z/Arch PGM NEW PSW - part 1
-r 1d8=00000000DEADDEAD  #  z/Arch PGM NEW PSW - part 2 (address)
-*
-* ----------------------------------------------------------------------------
-*
-r 200=1f00              #          SLR   R0,R0        Start clean
-r 202=41100001          #          LA    R1,1         Request z/Arch mode
-r 206=1f22              #          SLR   R2,R2        Start clean
-r 208=1f33              #          SLR   R3,R3        Start clean
-r 20a=ae020012          #          SIGP  R0,R2,X'12'  Request z/Arch mode
-r 20e=1f11              #          SLR   R1,R1        Start clean
-*
-r 210=41200000          #          LA    R2,0         Get our CPU number
-r 214=41400500          #          LA    R4,BEGIN0    Point to our loop
-r 218=404001ae          #          STH   R4,X'1AE'    Update restart PSW
-r 21c=ae020006          #          SIGP  R0,R2,X'6'   Restart our CPU
-r 220=b2b20310          #          LPSWE FAILPSW      How did we get here?!
-*
-* ----------------------------------------------------------------------------
-*
-r 300=0002000180000000  # GOODPSW  DC    0D'0',X'...  Success wait PSW part 1
-r 308=0000000000000000  #          DC    0D'0',X'...  Success wait PSW part 2
-r 310=0002000180000000  # FAILPSW  DC    0D'0',X'...  Failure wait PSW part 1
-r 318=00000000EEEEEEEE  #          DC    0D'0',X'...  Failure wait PSW part 2
-*
-* ----------------------------------------------------------------------------
-*
-r $(asec0)=0000000$(secs0)  # SECONDS0 DC    F'n'     Seconds CPU should loop
-r $(asec1)=0000000$(secs1)  # SECONDS1 DC    F'n'     Seconds CPU should loop
-r $(asec2)=0000000$(secs2)  # SECONDS2 DC    F'n'     Seconds CPU should loop
-r $(asec3)=0000000$(secs3)  # SECONDS3 DC    F'n'     Seconds CPU should loop
-*
-* ----------------------------------------------------------------------------
-*
-r 500=41200001          # BEGIN0   LA    R2,1         Get next CPU number
-r 504=41400600          #          LA    R4,BEGIN1    Point to next loop
-r 508=404001ae          #          STH   R4,X'1AE'    Update restart PSW
-r 50c=ae020006          #          SIGP  R0,R2,X'6'   Restart the next CPU
-*
-r 510=b2050$(aend0)     #          STCK     ENDTIME0  Current TOD clock value
-r 514=58100$(aend0)     #          L     R1,ENDTIME0  High word = approx. secs
-r 518=5e100$(asec0)     #          AL    R1,SECONDS0  Add seconds we're to loop
-r 51c=50100$(aend0)     #          ST    R1,ENDTIME0  Save ending clock value
-r 520=b2050$(anow0)     # LOOP0    STCK     NOWTIME0  Current TOD clock value
-r 524=58100$(anow0)     #          L     R1,NOWTIME0  High word = approx. secs
-r 528=55100$(aend0)     #          CL    R1,ENDTIME0  Reached ending TOD yet?
-r 52c=47400520          #          BL          LOOP0  Not yet. Keep looping...
-r 530=92ff0$(flag0)     #          MVI   X'FF',FLAG0  Indicate our loop ended
-r 534=b2b20300          #          LPSWE GOODPSW      Our CPU is now finished
-*
-* ----------------------------------------------------------------------------
-*
-r 600=41200002          # BEGIN1   LA    R2,2         Get next CPU number
-r 604=41400700          #          LA    R4,BEGIN2    Point to next loop
-r 608=404001ae          #          STH   R4,X'1AE'    Update restart PSW
-r 60c=ae020006          #          SIGP  R0,R2,X'6'   Restart the next CPU
-*  
-r 610=b2050$(aend1)     #          STCK     ENDTIME1  Current TOD clock value
-r 614=58100$(aend1)     #          L     R1,ENDTIME1  High word = approx. secs
-r 618=5e100$(asec1)     #          AL    R1,SECONDS1  Add seconds we're to loop
-r 61c=50100$(aend1)     #          ST    R1,ENDTIME1  Save ending clock value
-r 620=b2050$(anow1)     # LOOP1    STCK     NOWTIME1  Current TOD clock value
-r 624=58100$(anow1)     #          L     R1,NOWTIME1  High word = approx. secs
-r 628=55100$(aend1)     #          CL    R1,ENDTIME1  Reached ending TOD yet?
-r 62c=47400620          #          BL          LOOP1  Not yet. Keep looping...
-r 630=92ff0$(flag1)     #          MVI   X'FF',FLAG1  Indicate our loop ended
-r 634=b2b20300          #          LPSWE GOODPSW      Our CPU is now finished
-*
-* ----------------------------------------------------------------------------
-*
-r 700=41200003          # BEGIN2   LA    R2,3         Get next CPU number
-r 704=41400800          #          LA    R4,BEGIN3    Point to next loop
-r 708=404001ae          #          STH   R4,X'1AE'    Update restart PSW
-r 70c=ae020006          #          SIGP  R0,R2,X'6'   Restart the next CPU
-*  
-r 710=b2050$(aend2)     #          STCK     ENDTIME2  Current TOD clock value
-r 714=58100$(aend2)     #          L     R1,ENDTIME2  High word = approx. secs
-r 718=5e100$(asec2)     #          AL    R1,SECONDS2  Add seconds we're to loop
-r 71c=50100$(aend2)     #          ST    R1,ENDTIME2  Save ending clock value
-r 720=b2050$(anow2)     # LOOP2    STCK     NOWTIME2  Current TOD clock value
-r 724=58100$(anow2)     #          L     R1,NOWTIME2  High word = approx. secs
-r 728=55100$(aend2)     #          CL    R1,ENDTIME2  Reached ending TOD yet?
-r 72c=47400720          #          BL          LOOP2  Not yet. Keep looping...
-r 730=92ff0$(flag2)     #          MVI   X'FF',FLAG2  Indicate our loop ended
-r 734=b2b20300          #          LPSWE GOODPSW      Our CPU is now finished
-*
-* ----------------------------------------------------------------------------
-*
-r 800=b2050$(aend3)     # BEGIN3   STCK     ENDTIME3  Current TOD clock value
-r 804=58100$(aend3)     #          L     R1,ENDTIME3  High word = approx. secs
-r 808=5e100$(asec3)     #          AL    R1,SECONDS3  Add seconds we're to loop
-r 80c=50100$(aend3)     #          ST    R1,ENDTIME3  Save ending clock value
-r 810=b2050$(anow3)     # LOOP3    STCK     NOWTIME3  Current TOD clock value
-r 814=58100$(anow3)     #          L     R1,NOWTIME3  High word = approx. secs
-r 818=55100$(aend3)     #          CL    R1,ENDTIME3  Reached ending TOD yet?
-r 81c=47400810          #          BL          LOOP3  Not yet. Keep looping...
-r 820=92ff0$(flag3)     #          MVI   X'FF',FLAG3  Indicate our loop ended
-r 824=b2b20300          #          LPSWE GOODPSW      Our CPU is now finished
-*
-* ----------------------------------------------------------------------------
-*
-r $(anow0)=0123456789abcdef00000000  # NOWTIME0 DC    D'0',F'0'
-r $(aend0)=0123456789abcdef00000000  # ENDTIME0 DC    D'0',F'0'
-*
-r $(anow1)=0123456789abcdef00000001  # NOWTIME1 DC    D'0',F'1'
-r $(aend1)=0123456789abcdef00000001  # ENDTIME1 DC    D'0',F'1'
-*
-r $(anow2)=0123456789abcdef00000002  # NOWTIME2 DC    D'0',F'2'
-r $(aend2)=0123456789abcdef00000002  # ENDTIME2 DC    D'0',F'2'
-*
-r $(anow3)=0123456789abcdef00000003  # NOWTIME3 DC    D'0',F'3'
-r $(aend3)=0123456789abcdef00000003  # ENDTIME3 DC    D'0',F'3'
-*
-* ----------------------------------------------------------------------------
-* Must be contiguous!
-*
-r $(flag0)=f0           # FLAG0   DC   X'F0'           Silly test ended flag
-r $(flag1)=f1           # FLAG1   DC   X'F1'           Silly test ended flag
-r $(flag2)=f2           # FLAG2   DC   X'F2'           Silly test ended flag
-r $(flag3)=f3           # FLAG3   DC   X'F3'           Silly test ended flag
-*
-* ----------------------------------------------------------------------------
-* Start the test and wait for completion...
-*
-runtest $(maxdur)
-*
-* ----------------------------------------------------------------------------
-*
-r $(anow0).c            # Show CPU 0 actual ending  TOD
-r $(aend0).c            # Show CPU 0 ending compare TOD
-*
-r $(anow1).c            # Show CPU 1 actual ending  TOD
-r $(aend1).c            # Show CPU 1 ending compare TOD
-*
-r $(anow2).c            # Show CPU 2 actual ending  TOD
-r $(aend2).c            # Show CPU 2 ending compare TOD
-*
-r $(anow3).c            # Show CPU 3 actual ending  TOD
-r $(aend3).c            # Show CPU 3 ending compare TOD
-*
-* ----------------------------------------------------------------------------
-*
-*Compare
-r $(flag0)-$(flag3)
-*Want FFFFFFFF
-*
-* ----------------------------------------------------------------------------
-*
-*Done
-*
-* ----------------------------------------------------------------------------
-*Testcase runtest#2: all cpus 1-4 seconds
-* ----------------------------------------------------------------------------
-*
-defsym  secs0     1     #  Loop duration in seconds for each CPU...
-defsym  secs1     2
-defsym  secs2     3
-defsym  secs3     4
-*
-defsym  maxdur   4.5    #  Expected test duration, give or take.
 *
 * ----------------------------------------------------------------------------
 *

commit 63a47cc3224be12323eced7a043719aa1ba675b9

Fix runtest breakage caused by 2c03ad1ad93afaa65fa5196085a6162ffcd13e51 item 2.

cpu.c
@@ -1461,14 +1461,29 @@ CPU_Wait (REGS* regs)
         wait_condition (&sysblk.sync_bc_cond, &sysblk.intlock);
     }
 
-    /* Let waiting script know all CPUs are now STOPPED */
-    obtain_lock( &sysblk.scrlock );
-    if (sysblk.scrtest && !sysblk.started_mask)
+    /*
+    ** Let test script know when a CPU either stops or loads a
+    ** disabled wait state PSW, but NOT when a CPU simply loads
+    ** an ENABLED wait PSW such as when it wishes to simply wait
+    ** for an I/O, External or other type of interrupt.
+    **
+    ** NOTE: we can't rely on just sysblk.started_mask since there
+    ** is no guarantee the last CPU to have its sysblk.started_mask
+    ** cleared will actually be the last CPU to reach this point.
+    */
+    if (WAITSTATE( &regs->psw ) && !IS_IC_DISABLED_WAIT_PSW( regs ))
+        ;   /* enabled wait: do nothing */
+    else
     {
-        sysblk.scrtest++;
-        broadcast_condition( &sysblk.scrcond );
+        obtain_lock( &sysblk.scrlock );
+        if (sysblk.scrtest)
+        {
+            sysblk.scrtest++;
+            broadcast_condition( &sysblk.scrcond );
+        }
+        release_lock( &sysblk.scrlock );
     }
-    release_lock( &sysblk.scrlock );
+
     /* Wait for interrupt */
     wait_condition (&regs->intcond, &sysblk.intlock);
 

script.c
@@ -1206,7 +1206,7 @@ int runtest( SCRCTL *pCtl, char *cmdline, char *args )
     /* Press the restart or start button to start the test */
 
     obtain_lock( &sysblk.scrlock );
-    sysblk.scrtest = 1;
+    sysblk.scrtest = 1; /*(reset)*/
     release_lock( &sysblk.scrlock );
 
     if (dostart)
@@ -1236,8 +1236,18 @@ int runtest( SCRCTL *pCtl, char *cmdline, char *args )
             return -1;
         }
 
-        /* Has the test finished yet? */
-        if (sysblk.scrtest > 1)
+        /*           Has the test completed yet?
+        **
+        ** Before test scripts are started the sysblk.scrtest
+        ** counter is always reset to '1' to indicate testing
+        ** mode is active (see further above). When each CPU
+        ** completes its test (by either stopping or loading
+        ** a disabled wait PSW) code in cpu.c then increments
+        ** sysblk.scrtest. Only when sysblk.scrtest has been
+        ** incremented past the number of configured CPUs is
+        ** the test then considered to be complete.
+        */
+        if (sysblk.scrtest > sysblk.cpus)
         {
             rc = 0;
             break;