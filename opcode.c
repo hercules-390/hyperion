d6c3cad1c42bce0b2a1dc4bad7fdcd7ac48a5392
Fish (David B. Trout) <fish@infidels.org>
2016-01-19 19:48:19 -0800

    Additional github issue #90 quick fix for 370-mode CLCLE support

--- opcode.c
+++ opcode.c
@@ -2263,7 +2263,7 @@ static zz_func opcode_table[0x100][GEN_MAXARCH] = {
  /*A6*/   GENx370x390x900 (execute_opcode_a6xx,a6xx,""),
  /*A7*/   GENx370x390x900 (execute_opcode_a7_x,a7_x,""),
  /*A8*/   GENx37Xx390x900 (move_long_extended,RS,"MVCLE"),
- /*A9*/   GENx370x390x900 (compare_logical_long_extended,RS,"CLCLE"),
+ /*A9*/   GENx37Xx390x900 (compare_logical_long_extended,RS,"CLCLE"),
  /*AA*/   GENx___x___x___ ,
  /*AB*/   GENx___x___x___ ,
  /*AC*/   GENx370x390x900 (store_then_and_system_mask,SI,"STNSM"),
