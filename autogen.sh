3ebbb44eecf633c631a986c72cfc859b28e399cd
John P. Hartmann <jphartmann@gmail.com>
2016-12-30 18:59:16 +0000

    Cleanup of autogen; 1Stop to add -q to configure.

--- autogen.sh
+++ autogen.sh
@@ -1,52 +1,66 @@
 #!/bin/sh
 
-case `echo "testing\c"; echo 1,2,3`,`echo -n testing; echo 1,2,3` in
-  *c*,-n*) ECHO_N= ECHO_C='
-' ECHO_T='      ' ;;
-  *c*,*  ) ECHO_N=-n ECHO_C= ECHO_T= ;;
-  *)       ECHO_N= ECHO_C='\c' ECHO_T= ;;
-esac
+oneutil ()
+{
+        doing=$1
+        echo "*** From $1:" >>autogen.log  &&
+        if [ $loud -eq 1 ] ; then echo "$1..." ; fi
+        $@ >>./autogen.log 2>&1
+        # Return value is the one from command above.  No more here, please.
+}
 
-cat <<EOF
-Note: if you do not see a 'All processing successfully completed.'
-message when this script completes, then something went wrong and
-you should examine the output to try and determine what it was that
-went wrong.
+loud=1
+if [ "$1" = "-q" ] ; then loud=0; fi
 
-EOF
+if [ $loud -eq 1 ] ; then
+echo "This script runs four utilities that the developers"
+echo "cannot be bothered to run for you.  It finishes with"
+echo "instructions on how to configure Hyperion manually."
+echo ""
+fi
 
-echo "*** From aclocal:" >autogen.log  &&
-echo $ECHO_N "aclocal...    $ECHO_C" && aclocal -I m4 -I autoconf >>./autogen.log 2>&1 && echo "OK.  (25% done)" &&
-echo "*** From autoheader:" >>autogen.log  &&
-echo $ECHO_N "autoheader... $ECHO_C" && autoheader                >>./autogen.log 2>&1 && echo "OK.  (50% done)" &&
-echo "*** From automake:" >>autogen.log  &&
-echo $ECHO_N "automake...   $ECHO_C" && automake --add-missing    >>./autogen.log 2>&1 && echo "OK.  (75% done)" &&
-echo "*** From autoconf:" >>autogen.log  &&
-echo $ECHO_N "autoconf...   $ECHO_C" && autoconf                  >>./autogen.log 2>&1 && echo "OK.  (100% done)"
+rm -f autogen.log
+oneutil aclocal -I m4 -I autoconf &&
+oneutil autoheader                &&
+oneutil automake --add-missing    &&
+oneutil autoconf
 
 R=$?
-if [ "$R" = 0 ]
+if [ $R -eq 0 ]
 then
+        if [ $loud -eq 1 ] ; then
       cat <<EOF
 
-All processing successfully completed.
+Automuck processing comple.
 
-Create the Hyperion build directory, if it does not exist, and then
-change directory to the Hyperion build directory and run configure
-in order to create a custom Makefile in the build directory structure
-that is suitable for your platform and environment.
+You are in the "source" directory presently.
 
-You can use the script "./DoConfigure" to do that using the defaults.
-Then change directory to the build directory and issue make, make check,
-and perhaps make install.
+You need to
+
+1.  Create a "build" directory where object modules &c are stored.
+    The default build directory is '../$(uname -m)/$(basename $PWD)'
+    in your present set-up, but you can use any name that is a nephew
+    of the source directory; that is, one to the left and down one.
+
+2.  Change directory to the build directory.
+
+3.  Run configure in the source directory: ../../$(basename $PWD)/configure
+
+4.  Run make in the build directory.
+
+You can use the script "1Stop" to create all prequisites and
+do all of above list, including running this procedure.
+
+1Stop accepts parameters for Hyperion configure.
 
 EOF
       exit 0
+      fi
 else
-      echo "FAILED!"
       echo
-      echo "The last 10 lines of "autogen.log" follows..."
+      echo "Automuck did not complete because $doing failed."
+      echo "The last 10 lines of \"autogen.log\" follow..."
       echo
       tail autogen.log
-      exit "$R"
+      exit $R
 fi
