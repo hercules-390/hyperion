d15e28f8ba22d9665f4535f596b9cb7ec077a7d2
Stephen Orso <stephen.orso@yahoo.com>
2018-05-03 07:16:41 -0400

    Editorial correction; added brief functional description.

--- CMake/Herc03_CopySharedLib.cmake
+++ CMake/Herc03_CopySharedLib.cmake
@@ -1,11 +1,24 @@
-# Herc02_ExtPackageBuild.cmake - Build an external package needed by
-#                                Hercules
+# Herc03_CopySharedLib.cmake - Copy one, two, or three shared libraries
+#                              from a source directory to a target
+#                              directory.
 
 #[[   Copyright 2017 by Stephen Orso.
 
       Distributed under the Boost Software License, Version 1.0.
       See accompanying file BOOST_LICENSE_1_0.txt or copy at
       http://www.boost.org/LICENSE_1_0.txt)
+]]
+
+#[[
+
+This CMake script is the target of Add_Custom_Command calls in
+Herc65_ExtPackageWinCopy.cmake.  The added commands ensure that shared
+libraries created by executable packages are copied to the Hercules
+binary directory when Hercules is built on Windows.
+
+This script and the commands are not needed on UNIX-like and macOS
+builds because the shared libraries are located using the RPATH
+setting in the calling executable or shared library.
 ]]
 
 if( IN2 )
